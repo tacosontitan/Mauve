87271e5eafe733228f369c26da902eead7c43880
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 12:33:19 -0500

    Created SmtpNetworkClientBuilder.

---
+++ Mauve/Net/Smtp/SmtpNetworkClientBuilder.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mauve.Net.Smtp
+{
+    internal class SmtpNetworkClientBuilder : INetworkClientBuilder<SmtpNetworkClient>
+    {
+
+        #region Fields
+
+        private bool _useDefaultCredentials;
+        private int _port;
+        private string _domain;
+        private string _username;
+        private string _password;
+
+        #endregion
+
+        #region Constructor
+
+        public SmtpNetworkClientBuilder()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public SmtpNetworkClient Build()
+        {
+            // Create the network information object.
+            var networkConnectionInfo = new NetworkConnectionInformation
+            {
+                Port = _port,
+                UseDefaultCredentials = _useDefaultCredentials
+            };
+
+            // Determine if specific credentials have been provided.
+            bool credentialsSpecified =
+                !string.IsNullOrWhiteSpace(_domain) ||
+                !string.IsNullOrWhiteSpace(_username) ||
+                !string.IsNullOrWhiteSpace(_password);
+
+            // If we're supposed to use default credentials and credentials have been specified, inform the consumer.
+            if (_useDefaultCredentials && credentialsSpecified)
+                throw new InvalidOperationException("Unable to use default credentials when credentials are specified.");
+
+            // Create the network credentials for the network information object.
+            NetworkCredential networkCredential = !string.IsNullOrWhiteSpace(_domain)
+                ? new NetworkCredential(_username, _password, _domain)
+                : new NetworkCredential(_username, _password);
+
+            // Assign the credential to the network information.
+            networkConnectionInfo.Credential = networkCredential;
+
+            // Create the network client.
+            return new SmtpNetworkClient
+            {
+                ConnectionInformation = networkConnectionInfo
+            };
+        }
+        public SmtpNetworkClientBuilder UsePort(int port)
+        {
+            _port = port;
+            return this;
+        }
+        public SmtpNetworkClientBuilder UseCredentials(string username, string password)
+        {
+            _username = username;
+            _password = password;
+            return this;
+        }
+        public SmtpNetworkClientBuilder UseDefaultCredentials()
+        {
+            _useDefaultCredentials = true;
+            return this;
+        }
+        public SmtpNetworkClientBuilder UseDomain(string domain)
+        {
+            _domain = domain;
+            return this;
+        }
+
+        #endregion
+
+    }
+}
