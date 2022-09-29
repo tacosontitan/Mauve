d22fa772807b6d5b09644895042baf8c1bd63720
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 13:21:27 -0500

    Updated for type parameter support on INetworkRequest<T>.

---
+++ Mauve/Net/NetworkClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+
+namespace Mauve.Net
+{
+    public abstract class NetworkClient<TRequest, TData> : INetworkClient<TRequest, TData> where TRequest : INetworkRequest<TData>
+    {
+
+        #region Public Methods
+
+        public virtual INetworkResponse<TData> Execute(TRequest request)
+        {
+            TData response = default;
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            string responseMessage = string.Empty;
+            try
+            {
+                response = ExecuteRequest(request);
+                statusCode = HttpStatusCode.OK;
+            } catch (Exception e)
+            {
+                responseMessage = e.Message;
+            }
+
+            // Return the network response.
+            return new NetworkResponse<TData>
+            {
+                Content = response,
+                StatusCode = statusCode,
+                Message = responseMessage
+            };
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected abstract TData ExecuteRequest(TRequest request);
+
+        #endregion
+
+    }
+}
