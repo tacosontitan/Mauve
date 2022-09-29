e442564f505dc22a036c52a447003515cd397790
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 09:08:23 -0500

    Added <inheritdoc/> per MSDN recommendation.

--- Mauve/Serialization/RawSerializationProvider.cs
+++ Mauve/Serialization/RawSerializationProvider.cs
@@ -5,6 +5,7 @@ namespace Mauve.Serialization
     /// <summary>
     /// Represents a <see cref="SerializationProvider"/> focused on serializing and deserializing data using <see cref="SerializationMethod.None"/>.
     /// </summary>
+    /// <inheritdoc/>
     internal class RawSerializationProvider : SerializationProvider
     {
 
