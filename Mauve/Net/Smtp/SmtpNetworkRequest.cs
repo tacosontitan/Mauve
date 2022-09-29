87d9b865ef8c7fc005863cacb4c0ee6d25adf82a
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 17:10:58 -0500

    Updated documentation. Standardized file.

--- Mauve/Net/Smtp/SmtpNetworkRequest.cs
+++ Mauve/Net/Smtp/SmtpNetworkRequest.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Mail;
 
 namespace Mauve.Net.Smtp
 {
+    /// <summary>
+    /// Represents an <see cref="INetworkRequest{T}"/> instance for sending <see cref="MailMessage"/> instances using <see cref="INetworkClient{TRequest, TIn, TOut}"/> implementations.
+    /// </summary>
+    /// <inheritdoc/>
     public class SmtpNetworkRequest : INetworkRequest<MailMessage>
     {
 
@@ -18,6 +17,10 @@ namespace Mauve.Net.Smtp
 
         #region Constructor
 
+        /// <summary>
+        /// Creates a new <see cref="SmtpNetworkRequest"/> using the specified <see cref="MailMessage"/>.
+        /// </summary>
+        /// <param name="message">The message this request is focused on sending.</param>
         public SmtpNetworkRequest(MailMessage message) => Data = message;
 
         #endregion
