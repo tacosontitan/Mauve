6e0e262c5a446111125b25e3dc1643da478f7c9c
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 13:19:50 -0500

    Updated signatures to account for type parameters in INetworkRequest<T>.

--- Mauve/Net/INetworkClient.cs
+++ Mauve/Net/INetworkClient.cs
@@ -1,10 +1,15 @@
 ﻿namespace Mauve.Net
 {
     /// <summary>
-    /// Represents an <see cref="INetworkClient"/> instance capable of executing <see cref="INetworkRequest"/> instances.
+    /// Represents an <see cref="INetworkClient{TRequest, TData}"/> instance capable of executing <see cref="INetworkRequest{TData}"/> instances.
     /// </summary>
-    public interface INetworkClient<T> where T : INetworkRequest
+    public interface INetworkClient<TRequest, TData> where TData : INetworkRequest<TData>
     {
-        INetworkResponse Execute(T request);
+        /// <summary>
+        /// Executes the specified <see cref="TRequest"/> instance across the network this <see cref="INetworkClient{TRequest, TData}"/> operates within.
+        /// </summary>
+        /// <param name="request">The <see cref="TRequest"/> to execute.</param>
+        /// <returns>Returns the <see cref="INetworkResponse{TData}"/> containing response information from the network this <see cref="INetworkClient{TRequest, TData}"/> operates within.</returns>
+        INetworkResponse<TData> Execute(TRequest request);
     }
 }
