6bba5c7ec54d081fc7a7ef5ef0db5d3d06832c0e
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 17:27:59 -0500

    Updated documentation.

--- Mauve/Net/NetworkConnectionInformation.cs
+++ Mauve/Net/NetworkConnectionInformation.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
 
 namespace Mauve.Net
 {
+    /// <summary>
+    /// Represents connection information for various forms of network communication.
+    /// </summary>
     public class NetworkConnectionInformation
     {
+        /// <summary>
+        /// Should default crednetials be utilized for the connection?
+        /// </summary>
         public bool UseDefaultCredentials { get; set; }
+        /// <summary>
+        /// The port this connection uses.
+        /// </summary>
         public int? Port { get; set; }
+        /// <summary>
+        /// The host of the connection (e.g. the IP Address, Server Name, Base URL, etc).
+        /// </summary>
         public string Host { get; set; }
+        /// <summary>
+        /// The focus of the connection (e.g. the endpoint, object name, etc).
+        /// </summary>
         public string Focus { get; set; }
+        /// <summary>
+        /// The credential to be utilized for connection.
+        /// </summary>
         public NetworkCredential Credential { get; set; }
     }
 }
