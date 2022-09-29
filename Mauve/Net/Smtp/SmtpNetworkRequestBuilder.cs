70b1328014b6ef07623b07dd97bf5905273e1de0
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 12:35:13 -0500

    Opted for IBuilder<T>.

--- Mauve/Net/Smtp/SmtpNetworkRequestBuilder.cs
+++ Mauve/Net/Smtp/SmtpNetworkRequestBuilder.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Mauve.Patterns;
+
 namespace Mauve.Net.Smtp
 {
-    internal class SmtpNetworkRequestBuilder : INetworkRequestBuilder<SmtpNetworkRequest>
+    internal class SmtpNetworkRequestBuilder : IBuilder<SmtpNetworkRequest>
     {
 
         #region Fields
