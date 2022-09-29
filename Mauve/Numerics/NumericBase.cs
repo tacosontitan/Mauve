30bc1813f34b1d1b36fa7a6e1b702787ee6da87b
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 11:00:45 -0500

    Added numeric base.

---
+++ Mauve/Numerics/NumericBase.cs
@@ -0,0 +1,69 @@
+﻿namespace Mauve.Numerics
+{
+    /// <summary>
+    /// Represents a <see cref="NumericBase"/> for specification of base.
+    /// </summary>
+    public enum NumericBase : int
+    {
+        /// <summary>
+        /// Represents an unspecified or invalid base.
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// 
+        /// </summary>
+        Binary = 2,
+        /// <summary>
+        /// 
+        /// </summary>
+        Ternary = 3,
+        /// <summary>
+        /// 
+        /// </summary>
+        Quaternary = 4,
+        /// <summary>
+        /// 
+        /// </summary>
+        Quinary = 5,
+        /// <summary>
+        /// 
+        /// </summary>
+        Senary = 6,
+        /// <summary>
+        /// 
+        /// </summary>
+        Septenary = 7,
+        /// <summary>
+        /// 
+        /// </summary>
+        Octal = 8,
+        /// <summary>
+        /// 
+        /// </summary>
+        Nonary = 9,
+        /// <summary>
+        /// 
+        /// </summary>
+        Decimal = 10,
+        /// <summary>
+        /// 
+        /// </summary>
+        Undecimal = 11,
+        /// <summary>
+        /// 
+        /// </summary>
+        Duodecimal = 12,
+        /// <summary>
+        /// 
+        /// </summary>
+        Hexadecimal = 16,
+        /// <summary>
+        /// 
+        /// </summary>
+        Vigesimal = 20,
+        /// <summary>
+        /// 
+        /// </summary>
+        Sexagesimal = 60
+    }
+}
