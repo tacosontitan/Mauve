2be9107af05e7ddc4c8e0077ef4053cd1396b83a
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 12:32:30 -0500

    Created signed object prototype.

---
+++ Mauve/Security/Signed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mauve.Security
+{
+    public class Signed<TData, TSignatureAuthority>
+    {
+
+        #region Properties
+
+        public TData Data { get; set; }
+        public Signature<TSignatureAuthority> Signature { get; set; }
+
+        #endregion
+
+    }
+}
