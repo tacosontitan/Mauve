9cdb236c97ff24b07181b8879c148a7b45522c6b
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 12:33:08 -0500

    Created NetworkInformation.

---
+++ Mauve/Net/NetworkConnectionInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mauve.Net
+{
+    public class NetworkConnectionInformation
+    {
+        public bool UseDefaultCredentials { get; set; }
+        public int? Port { get; set; }
+        public string Identifier { get; set; }
+        public string Focus { get; set; }
+        public NetworkCredential Credential { get; set; }
+    }
+}
