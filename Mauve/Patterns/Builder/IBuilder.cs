a71ad695e1ed9da92aa7bb6bc2171c794852fcd8
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 08:38:54 -0500

    Created IBuilder<T>.

---
+++ Mauve/Patterns/Builder/IBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace Mauve.Patterns.Builder
+{
+    /// <summary>
+    /// Represents an <see cref="IBuilder{T}"/> interface that defines a method of building an instace of <see cref="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type this interface will build.</typeparam>
+    public interface IBuilder<T>
+    {
+        /// <summary>
+        /// Builds a new instance of <see cref="T"/>.
+        /// </summary>
+        /// <returns>Returns a new instance of <see cref="T"/>.</returns>
+        T Build();
+    }
+}
