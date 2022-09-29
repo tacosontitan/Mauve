92e361c28187ffe99e133fd2d5c08b3c28ab68e9
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 12:33:40 -0500

    Created SmtpNetworkRequestBuilder.

---
+++ Mauve/Net/Smtp/SmtpNetworkRequestBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mauve.Net.Smtp
+{
+    internal class SmtpNetworkRequestBuilder : INetworkRequestBuilder<SmtpNetworkRequest>
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Public Methods
+
+        public SmtpNetworkRequest Build() => throw new NotImplementedException();
+
+        #endregion
+
+    }
+}
