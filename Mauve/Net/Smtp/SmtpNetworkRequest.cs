ad2c60bc8e30ef5ebae4f71160cc32a4877c8e34
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 13:22:24 -0500

    Updated request model to be compliant with interface.

--- Mauve/Net/Smtp/SmtpNetworkRequest.cs
+++ Mauve/Net/Smtp/SmtpNetworkRequest.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mauve.Net.Smtp
 {
-    public class SmtpNetworkRequest : INetworkRequest
+    public class SmtpNetworkRequest : INetworkRequest<MailMessage>
     {
+        public MailMessage Data { get; set; }
     }
 }
