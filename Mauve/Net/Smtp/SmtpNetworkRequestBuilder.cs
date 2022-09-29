85068bdba1d7cdb8faac5ef69d68e47692bb332b
tacosontitan <jkdavis1691@gmail.com>
2022-09-29 17:23:58 -0500

    Updated documentation.

--- Mauve/Net/Smtp/SmtpNetworkRequestBuilder.cs
+++ Mauve/Net/Smtp/SmtpNetworkRequestBuilder.cs
@@ -10,6 +10,10 @@ using Mauve.Serialization;
 
 namespace Mauve.Net.Smtp
 {
+    /// <summary>
+    /// Represents an implementation of <see cref="IBuilder{T}"/> that is capable of building <see cref="SmtpNetworkRequest"/> instances.
+    /// </summary>
+    /// <inheritdoc/>
     public class SmtpNetworkRequestBuilder : IBuilder<SmtpNetworkRequest>, IDisposable
     {
 
@@ -32,6 +36,9 @@ namespace Mauve.Net.Smtp
 
         #region Constructor
 
+        /// <summary>
+        /// Creates a new instance of <see cref="SmtpNetworkRequestBuilder"/>.
+        /// </summary>
         private SmtpNetworkRequestBuilder()
         {
             _bodyBuilder = new StringBuilder();
@@ -45,8 +52,17 @@ namespace Mauve.Net.Smtp
             _attachmentStreamWriters = new List<StreamWriter>();
             _headers = new Dictionary<string, string>();
         }
+        /// <summary>
+        /// Creates a new instance of <see cref="SmtpNetworkRequestBuilder"/>.
+        /// </summary>
+        /// <param name="from">The sender of the generated request.</param>
         public SmtpNetworkRequestBuilder(string from) : this() =>
             _fromAddress = new MailAddress(from);
+        /// <summary>
+        /// Creates a new instance of <see cref="SmtpNetworkRequestBuilder"/>.
+        /// </summary>
+        /// <param name="from">The sender of the generated request.</param>
+        /// <param name="to">The primary recipient of the generated request.</param>
         public SmtpNetworkRequestBuilder(string from, string to) : this(from) =>
             _toAddresses.Add(new MailAddress(to));
 
@@ -54,12 +70,33 @@ namespace Mauve.Net.Smtp
 
         #region Public Methods
 
+        /// <summary>
+        /// Attaches a file to the request.
+        /// </summary>
+        /// <param name="file">The file to attach.</param>
+        /// <returns>Returns the current <see cref="SmtpNetworkRequestBuilder"/> instance.</returns>
         public SmtpNetworkRequestBuilder Attach(string file)
         {
             _attachments.Add(new Attachment(file));
             return this;
         }
+        /// <summary>
+        /// Attaches raw data to the request.
+        /// </summary>
+        /// <typeparam name="T">The type of data to attach.</typeparam>
+        /// <param name="name">The name of the generated file.</param>
+        /// <param name="data">The data to attach.</param>
+        /// <remarks>This method uses <see cref="SerializationMethod.Json"/> to serialize the incoming data prior to attachment.</remarks>
+        /// <returns>Returns the current <see cref="SmtpNetworkRequestBuilder"/> instance.</returns>
         public SmtpNetworkRequestBuilder Attach<T>(string name, T data) => Attach(name, data, SerializationMethod.Json);
+        /// <summary>
+        /// Attaches raw data to the request.
+        /// </summary>
+        /// <typeparam name="T">The type of data to attach.</typeparam>
+        /// <param name="name">The name of the generated file.</param>
+        /// <param name="data">The data to attach.</param>
+        /// <param name="serializationMethod">The serialization method that should be utilized to serialize the incoming data prior to attachment.</param>
+        /// <returns>Returns the current <see cref="SmtpNetworkRequestBuilder"/> instance.</returns>
         public SmtpNetworkRequestBuilder Attach<T>(string name, T data, SerializationMethod serializationMethod)
         {
             string serializedData = data.Serialize(serializationMethod);
@@ -73,11 +110,20 @@ namespace Mauve.Net.Smtp
             _attachmentMemoryStreams.Add(memoryStream);
             return this;
         }
+        /// <summary>
+        /// Adds a blind carbon copy recipient to the request.
+        /// </summary>
+        /// <param name="address">The address of the recipient.</param>
+        /// <returns>Returns the current <see cref="SmtpNetworkRequestBuilder"/> instance.</returns>
         public SmtpNetworkRequestBuilder BlindCarbonCopy(string address)
         {
             _blindCarbonCopyAddresses.Add(new MailAddress(address));
             return this;
         }
+        /// <summary>
+        /// Builds a new <see cref="SmtpNetworkRequest"/> instance.
+        /// </summary>
+        /// <returns>Returns a new <see cref="SmtpNetworkRequest"/> instance.</returns>
         public SmtpNetworkRequest Build()
         {
             // Create the MailMessage object to be sent.
@@ -114,41 +160,80 @@ namespace Mauve.Net.Smtp
             // Create a new network request.
             return new SmtpNetworkRequest(message);
         }
+        /// <summary>
+        /// Adds a blind carbon copy recipient to the request.
+        /// </summary>
+        /// <param name="address">The address of the recipient.</param>
+        /// <returns>Returns the current <see cref="SmtpNetworkRequestBuilder"/> instance.</returns>
         public SmtpNetworkRequestBuilder CarbonCopy(string address)
         {
             _carbonCopyAddresses.Add(new MailAddress(address));
             return this;
         }
+        /// <summary>
+        /// Adds a subject to the request.
+        /// </summary>
+        /// <param name="subject">The subject of the request.</param>
+        /// <returns>Returns the current <see cref="SmtpNetworkRequestBuilder"/> instance.</returns>
         public SmtpNetworkRequestBuilder CreateSubject(string subject)
         {
             _subject = subject;
             return this;
         }
+        /// <summary>
+        /// Disposes of the <see cref="MemoryStream"/> and <see cref="StreamWriter"/> instances used to attach raw data to the request.
+        /// </summary>
         public void Dispose()
         {
             _attachmentMemoryStreams.ForEach(memoryStream => memoryStream.Dispose());
             _attachmentStreamWriters.ForEach(streamWriter => streamWriter.Dispose());
         }
+        /// <summary>
+        /// Adds a reply-to recipient to the request.
+        /// </summary>
+        /// <param name="address">The address of the recipient.</param>
+        /// <returns>Returns the current <see cref="SmtpNetworkRequestBuilder"/> instance.</returns>
         public SmtpNetworkRequestBuilder ReplyTo(string address)
         {
             _replyToAddresses.Add(new MailAddress(address));
             return this;
         }
+        /// <summary>
+        /// Adds a recipient to the request.
+        /// </summary>
+        /// <param name="address">The address of the recipient.</param>
+        /// <returns>Returns the current <see cref="SmtpNetworkRequestBuilder"/> instance.</returns>
         public SmtpNetworkRequestBuilder SendTo(string address)
         {
             _toAddresses.Add(new MailAddress(address));
             return this;
         }
+        /// <summary>
+        /// Adds a header to the request.
+        /// </summary>
+        /// <param name="name">The name of the header.</param>
+        /// <param name="value">The value for the header.</param>
+        /// <returns>Returns the current <see cref="SmtpNetworkRequestBuilder"/> instance.</returns>
         public SmtpNetworkRequestBuilder UsingHeader(string name, string value)
         {
             _headers.Add(name, value);
             return this;
         }
+        /// <summary>
+        /// Writes a specified value to the body of the request.
+        /// </summary>
+        /// <param name="value">The value to write to the request.</param>
+        /// <returns>Returns the current <see cref="SmtpNetworkRequestBuilder"/> instance.</returns>
         public SmtpNetworkRequestBuilder Write(string value)
         {
             _ = _bodyBuilder.Append(value);
             return this;
         }
+        /// <summary>
+        /// Writes a specified value to the body of the request as a line.
+        /// </summary>
+        /// <param name="value">The value to write to the request.</param>
+        /// <returns>Returns the current <see cref="SmtpNetworkRequestBuilder"/> instance.</returns>
         public SmtpNetworkRequestBuilder WriteLine(string value)
         {
             _ = _bodyBuilder.AppendLine(value);
