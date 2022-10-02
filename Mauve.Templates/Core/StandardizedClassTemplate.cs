49ad7df4c8b232910ab7161d187d3366f980c747
tacosontitan <jkdavis1691@gmail.com>
2022-10-02 11:24:34 -0500

    Added class template.

---
+++ Mauve.Templates/Core/StandardizedClassTemplate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// Represents a <see cref="$safeitemname$"/>.
+    /// </summary>
+    internal sealed class $safeitemname$
+    {
+
+        #region Constants
+    
+        #endregion
+    
+        #region Fields
+    
+        #endregion
+    
+        #region Properties
+    
+        #endregion
+    
+        #region Constructor
+    
+        /// <summary>
+        /// Creates a new instance of <see cref="$safeitemname$"/>.
+        /// </summary>
+        public $safeitemname$() { }
+    
+        #endregion
+        
+        #region Public Methods
+        
+        #endregion
+        
+        #region Internal Methods
+        
+        #endregion
+        
+        #region Protected Internal Methods
+        
+        #endregion
+        
+        #region Protected Methods
+        
+        #endregion
+        
+        #region Private Methods
+        
+        #endregion
+        
+    }
+}
