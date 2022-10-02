6b1a3a0c88c543705d3ea3c5a5019f1c71a4a47b
tacosontitan <jkdavis1691@gmail.com>
2022-10-02 11:24:26 -0500

    Added interface template.

---
+++ Mauve.Templates/Core/InterfaceTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// Represents an <see langword="interface"/> for ...
+    /// </summary>
+    internal interface $safeitemname$
+    {
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+    }
+}
