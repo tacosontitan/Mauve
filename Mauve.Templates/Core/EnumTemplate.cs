64734aa49e2acdcd6312ab65b279c679154260b8
tacosontitan <jkdavis1691@gmail.com>
2022-10-02 11:23:42 -0500

    Created enum template.

---
+++ Mauve.Templates/Core/EnumTemplate.cs
@@ -0,0 +1,13 @@
+﻿namespace $rootnamespace$
+{
+    /// <summary>
+    /// Represents a <see cref="$safeitemname$"/>.
+    /// </summary>
+    internal enum $safeitemname$
+    {
+        /// <summary>
+        /// Represents an unspecified or unsupported value.
+        /// </summary>
+        None = 0
+    }
+}
