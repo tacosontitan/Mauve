45842b7dc838cee4178152089d0edc0ac3c0a3b9
tacosontitan <jkdavis1691@gmail.com>
2022-10-02 12:15:00 -0500

    Added IExecutableT.

---
+++ Mauve/IExecutableT.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Mauve
+{
+    /// <summary>
+    /// Represents an <see langword="interface"/> that exposes an execute method which returns data.
+    /// </summary>
+    public interface IExecutable<T>
+    {
+        T Execute();
+        Task<T> ExecuteAsync();
+    }
+}
