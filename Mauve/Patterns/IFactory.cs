3f0ecdff1f8e7fe43cd9d8c31d8f031288db9675
tacosontitan <jkdavis1691@gmail.com>
2022-10-02 11:46:55 -0500

    Created a factory interface.

---
+++ Mauve/Patterns/IFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Mauve.Patterns
+{
+    /// <summary>
+    /// Represents an <see langword="interface"/> that defines the basics of the <see href="https://en.wikipedia.org/wiki/Factory_method_pattern">factory pattern</see>.
+    /// </summary>
+    /// <typeparam name="T">The type of object this factory creates.</typeparam>
+    public interface IFactory<T>
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="T"/>.
+        /// </summary>
+        /// <returns>Returns a new instance of <see cref="T"/>.</returns>
+        T Create();
+    }
+}
