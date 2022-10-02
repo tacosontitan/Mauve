5bd454e7e1446e9f0f34d9bbbbed70e7dc05fb29
tacosontitan <jkdavis1691@gmail.com>
2022-10-02 11:24:13 -0500

    Created flag enum template.

---
+++ Mauve.Templates/Core/FlagEnumTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// Represents a <see cref="$safeitemname$"/>.
+    /// </summary>
+    [Flags]
+    internal enum $safeitemname$ : long
+    {
+        /// <summary>
+        /// Represents an unspecified or unsupported value.
+        /// </summary>
+        None = 0
+    }
+}
