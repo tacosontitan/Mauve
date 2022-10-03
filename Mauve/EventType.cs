845bd640d53fa2c496f74a27fa4580b8d0a426dc
tacosontitan <jkdavis1691@gmail.com>
2022-10-03 10:54:01 -0500

    Created event type enum.

---
+++ Mauve/EventType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Mauve
+{
+    [Flags]
+    public enum EventType : long
+    {
+        /// <summary>
+        /// Represents an unspecified or unsupported <see cref="EventType"/>.
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// Represents an event indicating that consumers should be alerted.
+        /// </summary>
+        Alert = 1L << 0,
+        /// <summary>
+        /// Represents an event indicating a runtime exception.
+        /// </summary>
+        Exception = 1L << 1,
+        /// <summary>
+        /// Represents an event indicating an error.
+        /// </summary>
+        Error = 1L << 2,
+        /// <summary>
+        /// Represents an event indicating a warning.
+        /// </summary>
+        Warning = 1L << 3,
+        /// <summary>
+        /// Represents an event indicating success.
+        /// </summary>
+        Success = 1L << 4,
+        /// <summary>
+        /// Represents an event conveying general information.
+        /// </summary>
+        Information = 1L << 5,
+        /// <summary>
+        /// Represents an event conveying only progress information.
+        /// </summary>
+        Progress = 1L << 6,
+        /// <summary>
+        /// Represents a diagnostic event.
+        /// </summary>
+        Diagnostic = 1L << 62,
+    }
+}
