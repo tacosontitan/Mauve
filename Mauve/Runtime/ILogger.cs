12f5b0fe2dfec84be3095ca615b2e0ddd47aa29b
tacosontitan <jkdavis1691@gmail.com>
2022-10-03 10:54:22 -0500

    Created logger interface.

---
+++ Mauve/Runtime/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Mauve.Runtime
+{
+    /// <summary>
+    /// Represents an <see langword="interface"/> that exposes generics enabled methods for logging.
+    /// </summary>
+    public interface ILogger<T>
+    {
+        void Log(T input);
+        Task LogAsync(T input);
+    }
+}
