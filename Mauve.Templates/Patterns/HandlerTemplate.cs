36b98ee5f72127f5d62c9120f41bc2149fc79843
tacosontitan <jkdavis1691@gmail.com>
2022-10-05 08:25:53 -0500

    Created a handler template.

---
+++ Mauve.Templates/Patterns/HandlerTemplate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mauve.Patterns;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="Handler{T}"/> that...
+    /// </summary>
+    /// <inheritdoc/>
+    internal sealed class $safeitemname$ : Handler<T>
+    {
+
+        #region Constructors
+
+        public $safeitemname$(T request) : base(request)
+        {
+        }
+        public $safeitemname$(T request, Handler<T> nextHandler) : base(request, nextHandler)
+        {
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override bool TryHandleRequest(T request) => throw new NotImplementedException();
+
+        #endregion
+
+    }
+}
