1f14e566935b46a18b3af79b1029d8431be191ca
tacosontitan <jkdavis1691@gmail.com>
2022-10-05 08:25:38 -0500

    Created a builder template.

---
+++ Mauve.Templates/Patterns/BuilderTemplate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mauve.Patterns;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IBuilder{T}"/> that...
+    /// </summary>
+    /// <inheritdoc/>
+    internal sealed class $safeitemname$ : IBuilder<T>
+    {
+
+        #region Public Methods
+
+        public T Build() => throw new NotImplementedException();
+
+        #endregion
+
+    }
+}
