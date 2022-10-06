23f151101b0c2614431e2d3b9000aef84bf5468a
tacosontitan <jkdavis1691@gmail.com>
2022-10-05 23:24:29 -0500

    Created dependency descriptor.

---
+++ Mauve/Patterns/DependencyDescriptor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mauve.Patterns
+{
+    public class DependencyDescriptor
+    {
+        public string Identifier { get; set; }
+        public object Value { get; set; }
+    }
+}
