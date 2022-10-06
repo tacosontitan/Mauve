526468a630fa3cbc725bd3753799e790f16652ed
tacosontitan <jkdavis1691@gmail.com>
2022-10-05 23:37:07 -0500

    Updated documentation.

--- Mauve/Patterns/IDependencyCollection.cs
+++ Mauve/Patterns/IDependencyCollection.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Microsoft.Extensions.DependencyInjection;
+﻿using System.Collections.Generic;
 
 namespace Mauve.Patterns
 {
+    /// <summary>
+    /// Represents an <see langword="interface"/> that exposes methods for manipulating a collection of dependencies.
+    /// </summary>
+    /// <remarks>Derives from <see cref="IList{T}"/>.</remarks>
     public interface IDependencyCollection : IList<DependencyDescriptor>
     {
+        /// <summary>
+        /// Adds an instance of the specified type to the collection.
+        /// </summary>
+        /// <typeparam name="T">The type of the instance to add.</typeparam>
+        /// <param name="singleton">The instance to add to the collection.</param>
+        /// <returns>Returns the current <see cref="IDependencyCollection"/>.</returns>
         IDependencyCollection AddSingleton<T>(T singleton);
     }
 }
