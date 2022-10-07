602f6cfdfb24dd214139cca9da8991bc8e88aebf
tacosontitan <jkdavis1691@gmail.com>
2022-10-07 10:36:48 -0500

    Created a cryptography provider shell for Caesar ciphers.

---
+++ Mauve/Security/CaesarCryptographyProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mauve.Security
+{
+    internal class CaesarCryptographyProvider : CryptographyProvider
+    {
+        public override T Decrypt<T>(string input) => throw new NotImplementedException();
+        public override void Dispose() => throw new NotImplementedException();
+        public override string Encrypt<T>(T input) => throw new NotImplementedException();
+    }
+}
