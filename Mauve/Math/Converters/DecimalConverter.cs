5ed8754c66eaf94a4fcc1ad1f455795c59bf3c55
tacosontitan <jkdavis1691@gmail.com>
2022-10-07 20:58:10 -0500

    Created decimal converter shell.

---
+++ Mauve/Math/Converters/DecimalConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mauve.Math.Converters
+{
+    public class DecimalConverter : NumericBaseConverter
+    {
+        protected override string FromBinary(string input) => throw new NotImplementedException();
+        protected override string FromDecimal(string input) => throw new NotImplementedException();
+        protected override string FromDuodecimal(string input) => throw new NotImplementedException();
+        protected override string FromHexadecimal(string input) => throw new NotImplementedException();
+        protected override string FromNonary(string input) => throw new NotImplementedException();
+        protected override string FromOctal(string input) => throw new NotImplementedException();
+        protected override string FromQuaternary(string input) => throw new NotImplementedException();
+        protected override string FromQuinary(string input) => throw new NotImplementedException();
+        protected override string FromSeptenary(string input) => throw new NotImplementedException();
+        protected override string FromSexagesimal(string input) => throw new NotImplementedException();
+        protected override string FromTernary(string input) => throw new NotImplementedException();
+        protected override string FromUndecimal(string input) => throw new NotImplementedException();
+        protected override string FromVigesimal(string input) => throw new NotImplementedException();
+        protected override string ToBinary(string input) => throw new NotImplementedException();
+        protected override string ToDecimal(string input) => throw new NotImplementedException();
+        protected override string ToDuodecimal(string input) => throw new NotImplementedException();
+        protected override string ToHexadecimal(string input) => throw new NotImplementedException();
+        protected override string ToNonary(string input) => throw new NotImplementedException();
+        protected override string ToOctal(string input) => throw new NotImplementedException();
+        protected override string ToQuaternary(string input) => throw new NotImplementedException();
+        protected override string ToQuinary(string input) => throw new NotImplementedException();
+        protected override string ToSeptenary(string input) => throw new NotImplementedException();
+        protected override string ToSexagesimal(string input) => throw new NotImplementedException();
+        protected override string ToTernary(string input) => throw new NotImplementedException();
+        protected override string ToUndecimal(string input) => throw new NotImplementedException();
+        protected override string ToVigesimal(string input) => throw new NotImplementedException();
+    }
+}
