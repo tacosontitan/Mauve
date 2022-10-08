b3c568098aaea31e0da67ee860279be091a4790f
tacosontitan <jkdavis1691@gmail.com>
2022-10-08 15:56:56 -0500

    Added index of method.

--- Mauve/Extensibility/IEnumerableExtensions.cs
+++ Mauve/Extensibility/IEnumerableExtensions.cs
@@ -19,5 +19,25 @@ namespace Mauve.Extensibility
             foreach (T item in collection)
                 action(item);
         }
+        /// <summary>
+        /// Gets the index of a specified <see cref="T"/> in the specified <see cref="IEnumerable{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of object contained in the <see cref="IEnumerable{T}"/> instance.</typeparam>
+        /// <param name="collection">The collection of objects to work with.</param>
+        /// <param name="searchValue">The element to search for.</param>
+        /// <returns>The index of a specified <see cref="T"/>, otherwise <c>-1</c>.</returns>
+        public static int IndexOf<T>(this IEnumerable<T> collection, T searchValue)
+        {
+            int index = 0;
+            foreach (T item in collection)
+            {
+                if (item.Equals(searchValue))
+                    return index;
+
+                index++;
+            }
+
+            return -1;
+        }
     }
 }
