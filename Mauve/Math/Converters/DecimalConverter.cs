0fdb7607735334efbc6c19a2f9842dcf3e218e09
tacosontitan <jkdavis1691@gmail.com>
2022-10-07 21:02:24 -0500

    Added basic conversions.

--- Mauve/Math/Converters/DecimalConverter.cs
+++ Mauve/Math/Converters/DecimalConverter.cs
@@ -8,12 +8,12 @@ namespace Mauve.Math.Converters
 {
     public class DecimalConverter : NumericBaseConverter
     {
-        protected override string FromBinary(string input) => throw new NotImplementedException();
-        protected override string FromDecimal(string input) => throw new NotImplementedException();
+        protected override string FromBinary(string input) => Convert.ToInt64(input, 2).ToString();
+        protected override string FromDecimal(string input) => Convert.ToInt64(input, 10).ToString();
         protected override string FromDuodecimal(string input) => throw new NotImplementedException();
-        protected override string FromHexadecimal(string input) => throw new NotImplementedException();
+        protected override string FromHexadecimal(string input) => Convert.ToInt64(input, 16).ToString();
         protected override string FromNonary(string input) => throw new NotImplementedException();
-        protected override string FromOctal(string input) => throw new NotImplementedException();
+        protected override string FromOctal(string input) => Convert.ToInt64(input, 8).ToString();
         protected override string FromQuaternary(string input) => throw new NotImplementedException();
         protected override string FromQuinary(string input) => throw new NotImplementedException();
         protected override string FromSeptenary(string input) => throw new NotImplementedException();
@@ -21,12 +21,12 @@ namespace Mauve.Math.Converters
         protected override string FromTernary(string input) => throw new NotImplementedException();
         protected override string FromUndecimal(string input) => throw new NotImplementedException();
         protected override string FromVigesimal(string input) => throw new NotImplementedException();
-        protected override string ToBinary(string input) => throw new NotImplementedException();
-        protected override string ToDecimal(string input) => throw new NotImplementedException();
+        protected override string ToBinary(string input) => Convert.ToString(long.Parse(input), 2);
+        protected override string ToDecimal(string input) => Convert.ToString(long.Parse(input), 10);
         protected override string ToDuodecimal(string input) => throw new NotImplementedException();
-        protected override string ToHexadecimal(string input) => throw new NotImplementedException();
+        protected override string ToHexadecimal(string input) => Convert.ToString(long.Parse(input), 16);
         protected override string ToNonary(string input) => throw new NotImplementedException();
-        protected override string ToOctal(string input) => throw new NotImplementedException();
+        protected override string ToOctal(string input) => Convert.ToString(long.Parse(input), 8);
         protected override string ToQuaternary(string input) => throw new NotImplementedException();
         protected override string ToQuinary(string input) => throw new NotImplementedException();
         protected override string ToSeptenary(string input) => throw new NotImplementedException();
