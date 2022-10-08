9ba39efe2fc2855a14ff614b03e38b16369d7171
tacosontitan <jkdavis1691@gmail.com>
2022-10-08 15:40:13 -0500

    Created interpreter template.

---
+++ Mauve.Templates/Patterns/InterpreterTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mauve.Patterns;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IInterpreter"/> that...
+    /// </summary>
+    /// <inheritdoc/>
+    internal sealed class $safeitemname$ : IInterpreter
+    {
+
+        #region Public Methods
+
+        /// <inheritdoc/>
+        public void Interpret<TIn, TOut>(IInterpretationContext<TIn, TOut> context);
+
+        #endregion
+
+    }
+}
