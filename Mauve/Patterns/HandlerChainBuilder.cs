a02196fc8926c951868bd22f497cfea0db23c01a
tacosontitan <jkdavis1691@gmail.com>
2022-10-07 21:45:17 -0500

    Created handler chain builder.

---
+++ Mauve/Patterns/HandlerChainBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace Mauve.Patterns
+{
+    public class HandlerChainBuilder<T> : IHandlerChainBuilder<T>
+    {
+
+        #region Fields
+
+        private readonly List<Handler<T>> _handlers;
+
+        #endregion
+
+        #region Constructor
+
+        public HandlerChainBuilder() => _handlers = new List<Handler<T>>();
+
+        #endregion
+
+        #region Public Methods
+
+        public Handler<T> Build() => BuildHandlerChainRecursive(0);
+        public IHandlerChainBuilder<T> Use(Handler<T> handler)
+        {
+            _handlers.Add(handler);
+            return this;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private Handler<T> BuildHandlerChainRecursive(int index)
+        {
+            // If we have no more handlers, we're done.
+            if (index > _handlers.Count)
+                return null;
+
+            // Get the current handler and set the next handler using recursion.
+            Handler<T> handler = _handlers[index];
+            handler.SetNextHandler(BuildHandlerChainRecursive(++index));
+            return handler;
+        }
+
+        #endregion
+
+    }
+}
