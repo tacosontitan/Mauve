17ed630881c2940a781b3b0f176dd47579780d42
tacosontitan <jkdavis1691@gmail.com>
2022-10-08 13:30:24 -0500

    Updated documentation.

--- Mauve/Security/Signed.cs
+++ Mauve/Security/Signed.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mauve.Security
+﻿namespace Mauve.Security
 {
+    /// <summary>
+    /// Represents a <see cref="class"/> for storing signed data.
+    /// </summary>
+    /// <typeparam name="TData">The type fo the signed data.</typeparam>
+    /// <typeparam name="TSignatureAuthority">The type of authority used to sign the data.</typeparam>
     public class Signed<TData, TSignatureAuthority>
     {
 
         #region Properties
 
+        /// <summary>
+        /// The signed data.
+        /// </summary>
         public TData Data { get; set; }
+        /// <summary>
+        /// The signature for the data.
+        /// </summary>
         public Signature<TSignatureAuthority> Signature { get; set; }
 
         #endregion
