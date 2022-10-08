1888bfba422b78c4095e9b98926813acf6821e6c
tacosontitan <jkdavis1691@gmail.com>
2022-10-08 14:23:04 -0500

    Removed validator interface.

--- Mauve/Security/ITokenBuilder.cs
+++ Mauve/Security/ITokenBuilder.cs
@@ -1,13 +1,12 @@
 ﻿
 using Mauve.Patterns;
-using Mauve.Validation;
 
 namespace Mauve.Security
 {
     /// <summary>
     /// Represents an <see cref="interface"/> that exposes methods for building a token.
     /// </summary>
-    public interface ITokenBuilder : IBuilder<byte[]>, IValidator
+    public interface ITokenBuilder : IBuilder<byte[]>
     {
         /// <summary>
         /// Signs the token.
