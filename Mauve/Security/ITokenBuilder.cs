e7c5cafb2089049df53ab15fc81d22a1d4d94207
tacosontitan <jkdavis1691@gmail.com>
2022-10-08 14:02:27 -0500

    Created a token builder interface.

---
+++ Mauve/Security/ITokenBuilder.cs
@@ -0,0 +1,17 @@
+﻿
+using Mauve.Patterns;
+using Mauve.Validation;
+
+namespace Mauve.Security
+{
+    /// <summary>
+    /// Represents an <see cref="interface"/> that exposes methods for building a token.
+    /// </summary>
+    public interface ITokenBuilder : IBuilder<byte[]>, IValidator
+    {
+        /// <summary>
+        /// Signs the token.
+        /// </summary>
+        void Sign();
+    }
+}
