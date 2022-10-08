d130faa5cf309fc65f2dabfabe6026bf97971b15
tacosontitan <jkdavis1691@gmail.com>
2022-10-08 16:12:18 -0500

    Added index of test.

--- Mauve.Tests/Core/Extensibility/IEnumerableExtensionTests.cs
+++ Mauve.Tests/Core/Extensibility/IEnumerableExtensionTests.cs
@@ -12,11 +12,21 @@ namespace Mauve.Tests.Core.Extensibility
     {
         [TestMethod()]
         [DataRow(new int[] { 2, 3, 5, 7 }, 17)]
-        public void SumCollection(IEnumerable<int> inputCollection, int expectedResult)
+        public void ForEachSum(IEnumerable<int> inputCollection, int expectedResult)
         {
             int sum = 0;
             inputCollection.ForEach(i => sum += i);
             Assert.AreEqual(expectedResult, sum);
+        }
+        [TestMethod()]
+        [DataRow(new int[] { 2, 3, 5, 7 }, 2, 0)]
+        [DataRow(new int[] { 2, 3, 5, 7 }, 3, 1)]
+        [DataRow(new int[] { 2, 3, 5, 7 }, 7, 3)]
+        [DataRow(new int[] { 2, 3, 5, 7 }, 9, -1)]
+        public void IndexOf(IEnumerable<int> inputCollection, int searchValue, int expectedResult)
+        {
+            int index = inputCollection.IndexOf(searchValue);
+            Assert.AreEqual(expectedResult, index);
         }
     }
 }
