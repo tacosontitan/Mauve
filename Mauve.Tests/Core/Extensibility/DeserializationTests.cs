5ce85415d48725cd00ddc4317ad7ca3669ea4291
tacosontitan <jkdavis1691@gmail.com>
2022-10-08 15:11:46 -0500

    Fixed binary deserialization test.

--- Mauve.Tests/Core/Extensibility/DeserializationTests.cs
+++ Mauve.Tests/Core/Extensibility/DeserializationTests.cs
@@ -20,8 +20,21 @@ namespace Mauve.Tests.Core.Extensibility
         [Serializable]
         public struct TestModel
         {
-            public int Id { get; set; }
-            public string Name { get; set; }
+
+            #region Fields
+
+            private int _id;
+            private string _name;
+
+            #endregion
+
+            #region Properties
+
+            public int Id { get => _id; set => _id = value; }
+            public string Name { get => _name; set => _name = value; }
+
+            #endregion
+
         }
 
         #endregion
@@ -29,7 +42,7 @@ namespace Mauve.Tests.Core.Extensibility
         #region Constants
 
         private const string RawData = @"Mauve.Tests.Core.Extensibility.DeserializationTests+TestModel";
-        private const string BinaryData = @"Ā\0\uff00\uffffǿ\0\0\0ఀ\u0002\0䵂畡敶吮獥獴‬敖獲潩㵮⸱⸰⸰ⰰ䌠汵畴敲渽略牴污‬畐汢捩敋呹歯湥渽汵լ\u0001\0䴽畡敶吮獥獴䌮牯\u2e65硅整獮扩汩瑩\u2e79敄敳楲污穩瑡潩呮獥獴含獥䵴摯汥\u0002\0㰓摉款彟慂正湩䙧敩摬㰕慎敭款彟慂正湩䙧敩摬ĀȈ\0Ā\0؀\u0003\0各獥୴";
+        private const string BinaryData = "Ā\0\uff00\uffffǿ\0\0\0ఀ\u0002\0䵂畡敶吮獥獴‬敖獲潩㵮⸱⸰⸰ⰰ䌠汵畴敲渽略牴污‬畐汢捩敋呹歯湥渽汵լ\u0001\0䴽畡敶吮獥獴䌮牯\u2e65硅整獮扩汩瑩\u2e79敄敳楲污穩瑡潩呮獥獴含獥䵴摯汥\u0002\0弃摩弅慮敭ĀȈ\0Ā\0؀\u0003\0各獥୴";
         private const string XmlData = @"<TestModel xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
   <Id>1</Id>
   <Name>Test</Name>
@@ -52,12 +65,12 @@ name: Test";
         {
             try
             {
-                TestModel deserializedData = input.Deserialize<TestModel>(serializationMethod);
                 var expectedResult = new TestModel
                 {
                     Id = 1,
                     Name = "Test"
                 };
+                TestModel deserializedData = input.Deserialize<TestModel>(serializationMethod);
                 Assert.AreEqual(expectedResult, deserializedData);
             } catch (Exception e)
             {
