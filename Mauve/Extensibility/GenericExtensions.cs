16babc816c8d09277f64d9c9753ffe2cf195b6fb
tacosontitan <jkdavis1691@gmail.com>
2022-10-10 10:39:34 -0500

    Added overload for get hash code.

--- Mauve/Extensibility/GenericExtensions.cs
+++ Mauve/Extensibility/GenericExtensions.cs
@@ -1,5 +1,9 @@
 ﻿
+using System.Security.Cryptography;
+using System.Text;
+
 using Mauve.Math;
+using Mauve.Security;
 using Mauve.Serialization;
 
 namespace Mauve.Extensibility
@@ -21,6 +25,33 @@ namespace Mauve.Extensibility
         /// <returns>Returns the hash code of the specified input in the specified <see cref="NumericBase"/>.</returns>
         public static string GetHashCode<T>(this T input, NumericBase numericBase) =>
             input.GetHashCode().ToString(numericBase);
+        /// <summary>
+        /// Gets the hash code of the input using the specified <see cref="HashType"/>, <see cref="Encoding"/>, and <see cref="SerializationMethod"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the input.</typeparam>
+        /// <param name="input">The input to get the hash code of.</param>
+        /// <param name="hashType">The <see cref="HashType"/> that should be used to compute the hash of the input.</param>
+        /// <param name="serializationMethod">The <see cref="SerializationMethod"/> that should be used to serialize the input prior to hashing.</param>
+        /// <returns>Returns the hash code of the specified input.</returns>
+        /// <remarks>Defaults to <see cref="object.GetHashCode()"/></remarks>
+        public static string GetHashCode<T>(this T input, HashType hashType, Encoding encoding, SerializationMethod serializationMethod)
+        {
+            string serializedInput = input.Serialize(serializationMethod);
+            HashAlgorithm hashAlgorithm;
+            switch (hashType)
+            {
+                case HashType.Md5: hashAlgorithm = new MD5CryptoServiceProvider(); break;
+                case HashType.Sha256: hashAlgorithm = new SHA256CryptoServiceProvider(); break;
+                case HashType.Sha384: hashAlgorithm = new SHA384CryptoServiceProvider(); break;
+                case HashType.Sha512: hashAlgorithm = new SHA512CryptoServiceProvider(); break;
+                case HashType.RipeMd: hashAlgorithm = new RIPEMD160Managed(); break;
+                default: return input.GetHashCode().ToString();
+            }
+
+            byte[] data = encoding.GetBytes(serializedInput);
+            byte[] hash = hashAlgorithm.ComputeHash(data);
+            return encoding.GetString(hash);
+        }
         /// <summary>
         /// Serializes the current state of the specified input utilizing the specified <see cref="SerializationMethod"/>.
         /// </summary>
