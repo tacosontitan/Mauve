dbb739e1daa24b7b126647526645c68eba6c678c
tacosontitan <jkdavis1691@gmail.com>
2022-10-11 20:51:14 -0500

    Added discoverable attribute.

---
+++ Mauve/DiscoverableAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mauve
+{
+    /// <summary>
+    /// Represents an <see cref="Attribute"/> that enables explicit discoverability.
+    /// </summary>
+    public class DiscoverableAttribute : Attribute
+    {
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new <see cref="DiscoverableAttribute"/> instance.
+        /// </summary>
+        public DiscoverableAttribute() :
+            base()
+        { }
+
+        #endregion
+
+    }
+}
