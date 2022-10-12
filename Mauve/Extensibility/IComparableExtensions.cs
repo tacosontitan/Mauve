b324d67b8ddde10d5c3e9da66afb111ba631b67e
tacosontitan <jkdavis1691@gmail.com>
2022-10-11 19:23:14 -0500

    Standardized file.

--- Mauve/Extensibility/IComparableExtensions.cs
+++ Mauve/Extensibility/IComparableExtensions.cs
@@ -31,6 +31,7 @@ namespace Mauve.Extensibility
         /// <param name="min">The minimum allowed value.</param>
         /// <param name="max">The maximum allowed value.</param>
         /// <returns>Returns true if the calling object's value is equal to or greater than the specified minimum value, while remaining less than or equal to the specified maximum value, otherwise false.</returns>
-        public static bool WithinRange(this IComparable value, IComparable min, IComparable max) => value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+        public static bool WithinRange(this IComparable value, IComparable min, IComparable max) =>
+            value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
     }
 }
