d764c17052bb938d4a1a4fa4ebec047b2f47b919
tacosontitan <jkdavis1691@gmail.com>
2022-10-13 19:01:51 -0500

    Created middleware interface.

---
+++ Mauve/Patterns/IMiddleware.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Mauve.Patterns
+{
+    /// <summary>
+    /// Represents an <see langword="interface"/> exposing methods for implementing middleware.
+    /// </summary>
+    public interface IMiddleware
+    {
+        /// <summary>
+        /// Invokes the middleware.
+        /// </summary>
+        /// <param name="next">The next middleware to utilize.</param>
+        Task InvokeAsync(MiddlewareDelegate next);
+    }
+}
