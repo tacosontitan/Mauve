204ff9188c62a0ae5befa19b8d7716ac53f66ad4
tacosontitan <jkdavis1691@gmail.com>
2022-10-13 23:57:46 -0500

    Created validation exception.

---
+++ Mauve/Validation/ValidationException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Mauve.Validation
+{
+    /// <summary>
+    /// Represents an <see cref="Exception"/> representing an issue with validation.
+    /// </summary>
+    public class ValidationException : Exception
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// The result of the validation process.
+        /// </summary>
+        public ValidationResult ValidationResult { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new <see cref="ValidationException"/> instance with the specified <see cref="ValidationResult"/> and message.
+        /// </summary>
+        /// <param name="validationResult">The result of the validation process.</param>
+        /// <param name="message">The message explaining what happened.</param>
+        public ValidationException(ValidationResult validationResult, string message) :
+            base(message) =>
+                ValidationResult = validationResult;
+
+        #endregion
+
+    }
+}
