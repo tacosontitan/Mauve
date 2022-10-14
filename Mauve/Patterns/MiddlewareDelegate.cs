057cf5d9fe83ba12b5399499206db7af85634bba
tacosontitan <jkdavis1691@gmail.com>
2022-10-13 19:01:41 -0500

    Created middleware delegates.

---
+++ Mauve/Patterns/MiddlewareDelegate.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Mauve.Patterns
+{
+    public delegate Task MiddlewareDelegate();
+    public delegate Task MiddlewareDelegate<T>(T input);
+    public delegate Task MiddlewareDelegate<T, U>(T t, U u);
+    public delegate Task MiddlewareDelegate<T, U, V>(T t, U u, V v);
+}
