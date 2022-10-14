34a7a3c39f545314c24696569d0670b5422ed3b1
tacosontitan <jkdavis1691@gmail.com>
2022-10-13 19:03:10 -0500

    Created synchronous awaiter.

---
+++ Mauve/Threading/SynchronousAwaiter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mauve.Threading
+{
+    public class SynchronousAwaiter : IDisposable
+    {
+
+        #region Public Methods
+
+        public void Dispose() => throw new NotImplementedException();
+
+        #endregion
+
+    }
+}
