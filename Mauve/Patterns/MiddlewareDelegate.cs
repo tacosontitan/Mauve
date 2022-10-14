39d678baf3dfe52aadbb69a1d99efa920b5565d3
tacosontitan <jkdavis1691@gmail.com>
2022-10-13 23:41:45 -0500

    Updated documentation.

--- Mauve/Patterns/MiddlewareDelegate.cs
+++ Mauve/Patterns/MiddlewareDelegate.cs
@@ -7,6 +7,20 @@ namespace Mauve.Patterns
     public delegate Task MiddlewareDelegate<T1, T2>(T1 t1, T2 t2);
     public delegate Task MiddlewareDelegate<T1, T2, T3>(T1 t1, T2 t2, T3 t3);
     public delegate Task MiddlewareDelegate<T1, T2, T3, T4>(T1 t1, T2 t2, T3 t3, T4 t4);
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T1">The first type the delegate works with.</typeparam>
+    /// <typeparam name="T2">The second type the delegate works with.</typeparam>
+    /// <typeparam name="T3">The third type the delegate works with.</typeparam>
+    /// <typeparam name="T4">The fourth type the delegate works with.</typeparam>
+    /// <typeparam name="T5">The fifth type the delegate works with.</typeparam>
+    /// <param name="t1">The first parameter for the delegate.</param>
+    /// <param name="t2">The second parameter for the delegate.</param>
+    /// <param name="t3">The third parameter for the delegate.</param>
+    /// <param name="t4">The fourth parameter for the delegate.</param>
+    /// <param name="t5">The fifth parameter for the delegate.</param>
+    /// <returns></returns>
     public delegate Task MiddlewareDelegate<T1, T2, T3, T4, T5>(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5);
     /// <summary>
     /// 
