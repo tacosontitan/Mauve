4443b02eadfb88f2b2122a640e6b1cd8479bd8be
tacosontitan <jkdavis1691@gmail.com>
2022-10-13 19:02:09 -0500

    Created middleware interface with two inputs.

---
+++ Mauve/Patterns/IMiddlewareTU.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Mauve.Patterns
+{
+    /// <summary>
+    /// Represents an <see langword="interface"/> exposing methods for implementing middleware.
+    /// </summary>
+    /// <typeparam name="T">Specifies the first type utilized by the middleware.</typeparam>
+    /// <typeparam name="U">Specifies the second type utilized by the middleware.</typeparam>
+    public interface IMiddleware<T, U>
+    {
+        /// <summary>
+        /// Invokes the middleware.
+        /// </summary>
+        /// <param name="t">The first input for the middleware.</param>
+        /// <param name="t">The second input for the middleware.</param>
+        /// <param name="next">The next middleware to utilize.</param>
+        Task Use(T t, U u, MiddlewareDelegate<T, U> next);
+    }
+}
