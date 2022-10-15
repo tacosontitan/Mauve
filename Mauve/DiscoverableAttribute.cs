ae336cc89e3ae45d9090e89881f7026f0465bef6
tacosontitan <jkdavis1691@gmail.com>
2022-10-15 14:07:37 -0500

    Added properties to the discoverable attribute.

--- Mauve/DiscoverableAttribute.cs
+++ Mauve/DiscoverableAttribute.cs
@@ -8,6 +8,21 @@ namespace Mauve
     public class DiscoverableAttribute : Attribute
     {
 
+        #region Properties
+
+        /// <summary>
+        /// The name of the object marked as discoverable.
+        /// </summary>
+        /// <remarks>This property is intended for display purposes.</remarks>
+        public string Name { get; set; }
+        /// <summary>
+        /// Description of the object marked as discoverable.
+        /// </summary>
+        /// <remarks>This property is intended for display purposes.</remarks>
+        public string Description { get; set; }
+
+        #endregion
+
         #region Constructor
 
         /// <summary>
@@ -16,6 +31,22 @@ namespace Mauve
         public DiscoverableAttribute() :
             base()
         { }
+        /// <summary>
+        /// Creates a new <see cref="DiscoverableAttribute"/> instance with the specified name.
+        /// </summary>
+        /// <param name="name">The name of the object marked as discoverable.</param>
+        public DiscoverableAttribute(string name) =>
+            Name = name;
+        /// <summary>
+        /// Creates a new <see cref="DiscoverableAttribute"/> instance with the specified name and description.
+        /// </summary>
+        /// <param name="name">The name of the object marked as discoverable.</param>
+        /// <param name="description">Description of the object marked as discoverable.</param>
+        public DiscoverableAttribute(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
 
         #endregion
 
