44d7992a466223e1cec50245c8b0d6255df8bd7f
tacosontitan <jkdavis1691@gmail.com>
2022-10-15 13:56:08 -0500

    Created validator class.

---
+++ Mauve/Validation/Validator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace Mauve.Validation
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IValidatable"/> for the purpose of performing validation.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type of data the validator is validating.</typeparam>
+    public abstract class Validator<T> : IValidatable
+    {
+
+        #region Fields
+
+        private readonly T _input;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// A collection of <see cref="ValidationResult"/> representing the results of the validation process.
+        /// </summary>
+        public List<ValidationResult> Results { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new <see cref="Validator{T}"/> instance.
+        /// </summary>
+        /// <param name="input">The input to validate.</param>
+        public Validator(T input)
+        {
+            _input = input;
+            Results = new List<ValidationResult>();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Attempts to validate the input for the <see cref="Validator{T}"/> instance.
+        /// </summary>
+        /// <returns><see langword="true"/> if the input is valid, otherwise <see langword="false"/>.</returns>
+        public bool TryValidate()
+        {
+            try
+            {
+                Validate();
+                return true;
+            } catch
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Validates the input for the <see cref="Validator{T}"/> instance.
+        /// </summary>
+        public void Validate() =>
+            ValidateInput(_input);
+
+        #endregion
+
+        #region Protected Methods
+
+        protected abstract void ValidateInput(T input);
+
+        #endregion
+
+    }
+}
