4d4993d279226d960b3b239983c8525dc7842705
tacosontitan <jkdavis1691@gmail.com>
2022-10-16 12:46:43 -0500

    Created adapter interface.

---
+++ Mauve/Patterns/IAdapter.cs
@@ -0,0 +1,23 @@
+﻿namespace Mauve.Patterns
+{
+    /// <summary>
+    /// Represents an <see cref="interface"/> that exposes methods for conversion between two concrete types.
+    /// </summary>
+    /// <typeparam name="T1">Specifies the first type this adapter uses.</typeparam>
+    /// <typeparam name="T2">Specifies the second type this adapter uses.</typeparam>
+    internal interface IAdapter<T1, T2>
+    {
+        /// <summary>
+        /// Converts from the first type to the second.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        T2 Convert(T1 input);
+        /// <summary>
+        /// Converts from the second type to the first.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        T1 Convert(T2 input);
+    }
+}
