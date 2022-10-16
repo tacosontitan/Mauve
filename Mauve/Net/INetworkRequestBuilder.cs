64b9133248ae456584ca6bcebe4ac48dad2b37e3
tacosontitan <jkdavis1691@gmail.com>
2022-10-16 12:56:36 -0500

    Created request builder interface.

---
+++ Mauve/Net/INetworkRequestBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mauve.Patterns;
+
+namespace Mauve.Net
+{
+    public interface INetworkRequestBuilder<TRequest, TData> : IBuilder<TRequest>
+        where TRequest : INetworkRequest<TData>
+    {
+        /// <summary>
+        /// Adds the specified header to the <see cref="INetworkRequest{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">Specifies the type of data the value for the header is.</typeparam>
+        /// <param name="key">The key for the header.</param>
+        /// <param name="value">The value of the header.</param>
+        /// <returns>Returns the current builder instance.</returns>
+        INetworkRequestBuilder<TRequest, TData> UseHeader<T>(string key, T value);
+    }
+}
