5b04d6697a9e1e9da44441defc720f3a59e9e3c0
tacosontitan <jkdavis1691@gmail.com>
2022-10-16 13:24:08 -0500

    Created service t2

---
+++ Mauve/Runtime/Services/IServiceT2.cs
@@ -0,0 +1,11 @@
+﻿
+using Mauve.Patterns;
+
+namespace Mauve.Runtime.Services
+{
+    public interface IService<T1, T2>
+    {
+        IDependencyCollection Dependencies { get; set; }
+        MiddlewareDelegate<T1, T2> MiddlewareDelegate { get; set; }
+    }
+}
