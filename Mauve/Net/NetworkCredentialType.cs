c43cedb1e3551197c0641dc9caa77be30543afc0
tacosontitan <jkdavis1691@gmail.com>
2022-10-16 11:31:39 -0500

    Created network credential type.

---
+++ Mauve/Net/NetworkCredentialType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mauve.Net
+{
+    /// <summary>
+    /// Represents a collection of different types of network credentials.
+    /// </summary>
+    public enum NetworkCredentialType
+    {
+        None = 0,
+        Basic = 1,
+        BearerToken = 2,
+        ApiKey = 3,
+        Digest = 4,
+        Oauth2 = 5,
+        Hawk = 6,
+        Aws = 7
+    }
+}
