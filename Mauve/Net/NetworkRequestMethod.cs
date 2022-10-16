41469b947153d7be41abe2cafd1c1ae1e67c11f7
tacosontitan <jkdavis1691@gmail.com>
2022-10-16 11:31:48 -0500

    Created network request method.

---
+++ Mauve/Net/NetworkRequestMethod.cs
@@ -0,0 +1,29 @@
+﻿namespace Mauve.Net
+{
+    /// <summary>
+    /// Represents a collection of different network request method types.
+    /// </summary>
+    internal enum NetworkRequestMethod
+    {
+        /// <summary>
+        /// Represents an unspecified or unsupported <see cref="NetworkRequestMethod"/>.
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// Represents a network request that receives data from the client.
+        /// </summary>
+        Get = 1,
+        /// <summary>
+        /// Represents a network request that sends data to the client.
+        /// </summary>
+        Post = 2,
+        /// <summary>
+        /// Represents a network request that mutates data with the client.
+        /// </summary>
+        Put = 3,
+        /// <summary>
+        /// Represents a network request that deletes data with the client.
+        /// </summary>
+        Delete = 4,
+    }
+}
