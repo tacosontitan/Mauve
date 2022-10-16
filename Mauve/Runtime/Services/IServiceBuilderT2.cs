4f9560960b7d4ed05f73509b0b46cfee4349e70e
tacosontitan <jkdavis1691@gmail.com>
2022-10-16 13:26:56 -0500

    Created service builder t2.

---
+++ Mauve/Runtime/Services/IServiceBuilderT2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Mauve.Patterns;
+
+namespace Mauve.Runtime
+{
+    public interface IServiceBuilder<T1, T2>
+    {
+        IServiceBuilder AddSingleton<T>(string alias, T instance);
+        IServiceBuilder AddSingleton<T>(T instance);
+        IServiceBuilder AddSingleton(string alias, Type type, object instance);
+        IServiceBuilder AddSingleton(Type type, object instance);
+        IServiceBuilder Run(IMiddleware<T1, T2> middleware);
+        IServiceBuilder Use(IMiddleware<T1, T2> middleware);
+    }
+}
