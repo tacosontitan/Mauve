c66b6cb8c2419bf0afd55ed94878060c2c2a3ec9
tacosontitan <jkdavis1691@gmail.com>
2022-10-16 11:32:07 -0500

    Created network credential base class.

---
+++ Mauve/Net/NetworkCredential.cs
@@ -0,0 +1,17 @@
+﻿namespace Mauve.Net
+{
+    /// <summary>
+    /// Represents a simplified definition of a network credential.
+    /// </summary>
+    public class NetworkCredential
+    {
+        /// <summary>
+        /// The type of this credential.
+        /// </summary>
+        public NetworkCredentialType Type { get; set; }
+        /// <summary>
+        /// The value of this credential.
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
