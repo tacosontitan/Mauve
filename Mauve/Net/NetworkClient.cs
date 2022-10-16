c2fbca4f7fbfcc0c9edea1b265ebd56ce3d0f0dc
tacosontitan <jkdavis1691@gmail.com>
2022-10-16 11:34:07 -0500

    Updated network client to adhere to new interface.

--- Mauve/Net/NetworkClient.cs
+++ Mauve/Net/NetworkClient.cs
@@ -2,35 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 using Mauve.Extensibility;
 
 namespace Mauve.Net
 {
-    /// <inheritdoc/>
-    public abstract class NetworkClient<TRequest, TData> :
-        NetworkClient<TRequest, TData, TData> where TRequest : INetworkRequest<TData>
-    {
-        // This class is up for debate as we're not a big fan of a new class definition with no unique members.
-    }
     /// <summary>
-    /// Represents a <see langword="abstract"/> implementation of <see cref="INetworkClient{TRequest, TIn, TOut}"/> that handles exceptions and response codes natively.
+    /// Represents a <see langword="abstract"/> implementation of <see cref="INetworkClient"/> that handles exceptions and response codes natively.
     /// </summary>
     /// <inheritdoc/>
-    public abstract class NetworkClient<TRequest, TIn, TOut> : INetworkClient<TRequest, TIn, TOut> where TRequest : INetworkRequest<TIn>
+    public abstract class NetworkClient : INetworkClient
     {
+
+        #region Properties
+
+        public Uri BaseUri { get; set; }
+
+        #endregion
+
+        public NetworkClient(string baseUri) :
+            this(new Uri(baseUri))
+        { }
+        public NetworkClient(Uri baseUri) =>
+            BaseUri = baseUri;
 
         #region Public Methods
 
-        public virtual INetworkResponse<TOut> Execute(TRequest request)
+        public virtual INetworkResponse<TOut> Execute<TRequest, TIn, TOut>(TRequest request)
+            where TRequest : INetworkRequest<TIn>
         {
             TOut response = default;
             HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
             string responseMessage = string.Empty;
             try
             {
-                response = ExecuteRequest(request);
+                response = ExecuteRequest<TRequest, TIn, TOut>(request);
                 statusCode = HttpStatusCode.OK;
+                responseMessage = "Success.";
             } catch (Exception e)
             {
                 IEnumerable<Exception> flattenedExceptionTree = e.Flatten();
@@ -46,17 +56,23 @@ namespace Mauve.Net
                 Message = responseMessage
             };
         }
+        public async Task<INetworkResponse<TOut>> ExecuteAsync<TRequest, TIn, TOut>(TRequest request)
+            where TRequest : INetworkRequest<TIn> =>
+                await ExecuteAsync<TRequest, TIn, TOut>(request, CancellationToken.None);
+        public async Task<INetworkResponse<TOut>> ExecuteAsync<TRequest, TIn, TOut>(TRequest request, CancellationToken cancellationToken)
+            where TRequest : INetworkRequest<TIn> =>
+                await Task.Run(() => Execute<TRequest, TIn, TOut>(request), cancellationToken);
 
         #endregion
 
         #region Protected Methods
 
         /// <summary>
-        /// Executes the specified <see cref="TRequest"/>.
+        /// Executes the specified <see cref="INetworkRequest{T}"/>.
         /// </summary>
         /// <param name="request">The request to execute.</param>
         /// <returns>Returns the appropriate <see cref="TOut"/> instance representing the result of executing the specified <see cref="TRequest"/>.</returns>
-        protected abstract TOut ExecuteRequest(TRequest request);
+        protected abstract TOut ExecuteRequest<TRequest, TIn, TOut>(TRequest request) where TRequest : INetworkRequest<TIn>;
 
         #endregion
 
