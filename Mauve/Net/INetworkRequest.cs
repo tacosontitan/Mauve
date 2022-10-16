42648d9994c413f12b700b7e0f3a4018454b6efc
tacosontitan <jkdavis1691@gmail.com>
2022-10-16 12:53:30 -0500

    Added method and headers.

--- Mauve/Net/INetworkRequest.cs
+++ Mauve/Net/INetworkRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace Mauve.Net
+﻿using System.Collections.Generic;
+
+namespace Mauve.Net
 {
     /// <summary>
     /// Represents an <see cref="INetworkRequest{T}"/> instance utilized for requesting data over a network.
@@ -6,8 +8,16 @@
     public interface INetworkRequest<T>
     {
         /// <summary>
+        /// The method utilized by this request.
+        /// </summary>
+        NetworkRequestMethod Method { get; set; }
+        /// <summary>
         /// The data for this specific request.
         /// </summary>
         T Data { get; set; }
+        /// <summary>
+        /// The headers utilized in the request.
+        /// </summary>
+        Dictionary<string, object> Headers { get; set; }
     }
 }
