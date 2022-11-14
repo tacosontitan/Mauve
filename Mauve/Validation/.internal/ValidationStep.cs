3ab001f44345b3312ab87aee7af9598d690d6c71
tacosontitan <jkdavis1691@gmail.com>
2022-11-13 19:39:19 -0600

    Created validation step.

---
+++ Mauve/Validation/.internal/ValidationStep.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Mauve.Patterns;
+
+namespace Mauve.Validation
+{
+    internal class ValidationStep<T> : Handler<Func<T, bool>>
+    {
+
+        #region Fields
+
+        private readonly T _input;
+
+        #endregion
+
+        #region Constructor
+
+        public ValidationStep(T input, Func<T, bool> request) :
+            base(request) =>
+            _input = input;
+        public ValidationStep(T input, Func<T, bool> request, ValidationStep<T> nextStep) :
+            base(request, nextStep) =>
+            _input = input;
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override bool TryHandleRequest(Func<T, bool> request) =>
+            request(_input);
+
+        #endregion
+
+    }
+}
