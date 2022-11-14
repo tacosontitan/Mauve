0c7be0f1b073a8fa9a4b9b4d24be9559e15b581c
tacosontitan <jkdavis1691@gmail.com>
2022-11-13 18:59:25 -0600

    Created validation ruleset.

---
+++ Mauve/Validation/ValidationRuleset.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Mauve.Validation
+{
+    /// <summary>
+    /// Represents a collection of <see cref="ValidationRule"/> instances to be applied during validation.
+    /// </summary>
+    public class ValidationRuleset
+    {
+
+        #region Fields
+
+        private readonly List<ValidationRule> _rules;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new <see cref="ValidationRuleset"/> instance.
+        /// </summary>
+        public ValidationRuleset() =>
+            _rules = new List<ValidationRule>();
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Adds a new <see cref="ValidationRule"/> to the <see cref="ValidationRuleset"/>.
+        /// </summary>
+        /// <param name="rule">The <see cref="ValidationRule"/> to add to the ruleset.</param>
+        public void Add(ValidationRule rule) =>
+            _rules.Add(rule);
+        /// <summary>
+        /// Applies all of the <see cref="ValidationRule"/> instances within the ruleset.
+        /// </summary>
+        public void Apply() =>
+            _rules.ForEach(rule => rule.Apply());
+
+        #endregion
+
+    }
+}
