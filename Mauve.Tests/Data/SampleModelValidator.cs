ae7502e03e1c1dda15acd5680474c1704158b82f
tacosontitan <jkdavis1691@gmail.com>
2022-11-13 19:02:07 -0600

    Created sample model validator.

---
+++ Mauve.Tests/Data/SampleModelValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Mauve.Validation;
+
+namespace Mauve.Tests.Data
+{
+    internal class SampleModelValidator : Validator<SampleModel>
+    {
+        public SampleModelValidator(SampleModel input) : base(input)
+        {
+        }
+        protected override void CreateRules(IValidationRuleBuilder<SampleModel> ruleBuilder)
+        {
+            _ = CreateRule(model => model.Timestamp)
+                .WhenNull().Throw(new ArgumentNullException("Timestamp is null."))
+                .WhenIn(DateTime.MinValue, DateTime.MaxValue).Throw(new ArgumentException("Timestamp is invalid."))
+                .When(timestamp => timestamp > DateTime.Now).Throw(new ArgumentException("Timestamp cannot be in the future."))
+                .WhenEqualTo(DateTime.Now).Throw(new ArgumentException("Timestamp is now."))
+                .WhenNotEqualTo(new DateTime(2020, 1, 1))
+                .Then(timestamp => Console.WriteLine(timestamp));
+        }
+    }
+}
