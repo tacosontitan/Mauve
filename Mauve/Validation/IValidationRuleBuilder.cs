6a4e6682482217275de2c22dd5fbe7e236ed05cc
tacosontitan <jkdavis1691@gmail.com>
2022-11-13 18:59:41 -0600

    Created validation rule builder interface.

---
+++ Mauve/Validation/IValidationRuleBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mauve.Validation
+{
+    public interface IValidationRuleBuilder<T>
+    {
+        IValidationRuleBuilder<T> Otherwise(Action<T> action);
+        IValidationRuleBuilder<T> Then(Action<T> action);
+        IValidationRuleBuilder<T> Throw(Exception e);
+        IValidationRuleBuilder<T> Unless(Predicate<T> predicate);
+        IValidationRuleBuilder<T> When(Predicate<T> predicate);
+        IValidationRuleBuilder<T> WhenEqualTo(T value);
+        IValidationRuleBuilder<T> WhenIn(params T[] values);
+        IValidationRuleBuilder<T> WhenNotEqualTo(T value);
+        IValidationRuleBuilder<T> WhenNull();
+        IValidationRuleBuilder<T> WithMessage(Func<T, string> messageQuery);
+    }
+}
