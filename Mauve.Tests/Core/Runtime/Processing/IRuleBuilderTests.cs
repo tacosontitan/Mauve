071a69c619f9711a4bf36492a917033d998a57ac
tacosontitan <jkdavis1691@gmail.com>
2022-11-19 11:38:21 -0600

    Created basic when then test.

--- Mauve.Tests/Core/Runtime/Processing/IRuleBuilderTests.cs
+++ Mauve.Tests/Core/Runtime/Processing/IRuleBuilderTests.cs
@@ -10,21 +10,16 @@ namespace Mauve.Tests.Core.Runtime.Processing
     public class IRuleBuilderTests
     {
         [TestMethod]
-        [DataRow(1, 1, false, DisplayName = "Single Action")]
-        [DataRow(11, 2, false, DisplayName = "Multiple Actions")]
-        [DataRow(7, 3, false, DisplayName = "Chained Actions")]
-        public void BasicRules(int input, int expectedActionCount, bool exceptionExpected)
+        [DataRow(1, false)]
+        [DataRow(10, false)]
+        [DataRow(14, true)]
+        public void WhenThen(int input, bool expectedResult)
         {
-            int actionsExecuted = 0;
-            bool exceptionOccurred = false;
+            bool thenInvoked = false;
             var ruleBuilder = new RuleBuilder<int>();
             Rule<int> testRule = ruleBuilder
-                .When(i => i > 10).Then(i => actionsExecuted++)
-                .When(i => i % 2 == 0).Then(i => actionsExecuted++)
-                .WhenIn(2, 4, 6, 8).Then(i => actionsExecuted++)
-                .WhenNotIn(3, 5, 9).Then(i => actionsExecuted++).Then(i => actionsExecuted++)
-                .When(i => i > 15).Then(i => actionsExecuted++)
-                .Unless(i => i % 6 == 0).Then(i => actionsExecuted++)
+                .When(i => i > 10)
+                .Then(i => thenInvoked = true)
                 .Build();
 
             try
@@ -32,11 +27,10 @@ namespace Mauve.Tests.Core.Runtime.Processing
                 testRule.Apply(input);
             } catch (Exception e)
             {
-                exceptionOccurred = true;
                 Assert.Fail(e.Message);
             }
-            Assert.AreEqual(expectedActionCount, actionsExecuted);
-            Assert.AreEqual(exceptionExpected, exceptionOccurred);
+
+            Assert.AreEqual(expectedResult, thenInvoked);
         }
     }
 }
