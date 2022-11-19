4e8569d47f5de1979e3b148a5627ae93cd1ae636
tacosontitan <jkdavis1691@gmail.com>
2022-11-19 10:36:45 -0600

    Updated rule to use action.

--- Mauve/Runtime/Processing/Rule.cs
+++ Mauve/Runtime/Processing/Rule.cs
@@ -1,25 +1,27 @@
-﻿namespace Mauve.Runtime.Processing
+﻿using System;
+
+namespace Mauve.Runtime.Processing
 {
     public class Rule<T>
     {
 
         #region Fields
 
-        private readonly RuleHandler<T> _ruleHandler;
+        private readonly Action<T> _action;
 
         #endregion
 
         #region Constructor
 
-        public Rule(RuleHandler<T> handler) =>
-            _ruleHandler = handler;
+        public Rule(Action<T> action) =>
+            _action = action;
 
         #endregion
 
         #region Public Methods
 
         public void Apply(T input) =>
-            _ruleHandler.Execute(input);
+            _action?.Invoke(input);
 
         #endregion
 
