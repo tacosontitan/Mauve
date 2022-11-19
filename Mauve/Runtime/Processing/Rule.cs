d93d803d4aaabc520cde42b3efe56adc79fd7378
tacosontitan <jkdavis1691@gmail.com>
2022-11-19 11:36:53 -0600

    Opted for recursive application in rules.

--- Mauve/Runtime/Processing/Rule.cs
+++ Mauve/Runtime/Processing/Rule.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Mauve.Extensibility;
 
 namespace Mauve.Runtime.Processing
 {
@@ -7,21 +11,42 @@ namespace Mauve.Runtime.Processing
 
         #region Fields
 
-        private readonly Action<T> _action;
+        private readonly List<Func<T, bool>> _functions;
 
         #endregion
 
         #region Constructor
 
-        public Rule(Action<T> action) =>
-            _action = action;
+        public Rule(IEnumerable<Func<T, bool>> functions) =>
+             _functions = new List<Func<T, bool>>(functions);
+        public Rule(params Func<T, bool>[] functions) =>
+            _functions = new List<Func<T, bool>>(functions);
 
         #endregion
 
         #region Public Methods
 
-        public void Apply(T input) =>
-            _action?.Invoke(input);
+        public void Apply(T input)
+        {
+            Func<T, bool> firstFunction = _functions.FirstOrDefault();
+            Func<T, bool> secondFunction = _functions.NextOrDefault(firstFunction);
+            ApplyRecursive(input, firstFunction, secondFunction);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ApplyRecursive(T input, Func<T, bool> currentFunction, Func<T, bool> nextFunction)
+        {
+            if (currentFunction?.Invoke(input) == true)
+            {
+                if (nextFunction is null)
+                    return;
+
+                ApplyRecursive(input, nextFunction, _functions.NextOrDefault(nextFunction));
+            }
+        }
 
         #endregion
 
