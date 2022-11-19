ca9aac04af9a39a0df1d8f3ad2807931fbd02f67
tacosontitan <jkdavis1691@gmail.com>
2022-11-18 19:27:15 -0600

    Added type parameter, created instruction model.

--- Mauve/Runtime/Processing/Rule.cs
+++ Mauve/Runtime/Processing/Rule.cs
@@ -1,17 +1,37 @@
-﻿namespace Mauve.Runtime.Processing
+﻿using System.Collections.Generic;
+
+namespace Mauve.Runtime.Processing
 {
-    public class Rule
+    public class Rule<T>
     {
 
+        #region Sub-Types
+
+        private class Instruction
+        {
+
+        }
+
+        #endregion
+
         #region Fields
+
+        private readonly List<Instruction> _instructions;
 
         #endregion
 
         #region Constructor
 
+        public Rule() =>
+            _instructions = new List<Instruction>();
+
         #endregion
 
         #region Public Methods
+
+        #endregion
+
+        #region Internal Methods
 
         #endregion
 
