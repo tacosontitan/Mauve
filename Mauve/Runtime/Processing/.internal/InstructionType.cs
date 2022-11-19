d7a33c07a4b5ee99c929e867ad742d90189fe0f8
tacosontitan <jkdavis1691@gmail.com>
2022-11-18 20:22:14 -0600

    Created instruction type enum.

---
+++ Mauve/Runtime/Processing/.internal/InstructionType.cs
@@ -0,0 +1,11 @@
+﻿namespace Mauve.Runtime.Processing
+{
+    internal enum InstructionType
+    {
+        None = 0,
+        When = 1,
+        Unless = 2,
+        Otherwise = 3,
+        Then = 4
+    }
+}
