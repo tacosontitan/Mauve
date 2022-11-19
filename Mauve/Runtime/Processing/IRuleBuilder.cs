9d4340cdd2501ad605454907bb1802e826830066
tacosontitan <jkdavis1691@gmail.com>
2022-11-18 19:09:21 -0600

    Added methods.

--- Mauve/Runtime/Processing/IRuleBuilder.cs
+++ Mauve/Runtime/Processing/IRuleBuilder.cs
@@ -1,8 +1,20 @@
-﻿using Mauve.Patterns;
+﻿using System;
+
+using Mauve.Patterns;
 
 namespace Mauve.Runtime.Processing
 {
-    internal interface IRuleBuilder : IBuilder<Rule>
+    internal interface IRuleBuilder<T> : IBuilder<Rule>
     {
+        IRuleBuilder<T> Otherwise(Action<T> action);
+        IRuleBuilder<T> Then(Action<T> action);
+        IRuleBuilder<T> Throw(Exception e);
+        IRuleBuilder<T> Unless(Predicate<T> predicate);
+        IRuleBuilder<T> When(Predicate<T> predicate);
+        IRuleBuilder<T> WhenEqualTo(T value);
+        IRuleBuilder<T> WhenIn(params T[] values);
+        IRuleBuilder<T> WhenNotEqualTo(T value);
+        IRuleBuilder<T> WhenNull();
+        IRuleBuilder<T> WithMessage(Func<T, string> messageQuery);
     }
 }
