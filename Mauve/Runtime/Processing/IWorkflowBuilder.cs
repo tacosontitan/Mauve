bac9cf4e7db6e3aa444f29a4acdadbd109b79a87
tacosontitan <jkdavis1691@gmail.com>
2022-11-18 19:03:40 -0600

    Derived from IBuilder.

--- Mauve/Runtime/Processing/IWorkflowBuilder.cs
+++ Mauve/Runtime/Processing/IWorkflowBuilder.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Mauve.Patterns;
 
 namespace Mauve.Runtime.Processing
 {
-    internal interface IWorkflowBuilder
+    internal interface IWorkflowBuilder : IBuilder<Workflow>
     {
     }
 }
