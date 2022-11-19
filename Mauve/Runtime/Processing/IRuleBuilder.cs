099c6eea898914050444e949515b2535679c3a46
tacosontitan <jkdavis1691@gmail.com>
2022-11-18 19:17:23 -0600

    Updated parameter and added documentation for with message method.

--- Mauve/Runtime/Processing/IRuleBuilder.cs
+++ Mauve/Runtime/Processing/IRuleBuilder.cs
@@ -65,6 +65,11 @@ namespace Mauve.Runtime.Processing
         /// </summary>
         /// <returns>The current <see cref="IRuleBuilder{T}"/> instance.</returns>
         IRuleBuilder<T> WhenNull();
-        IRuleBuilder<T> WithMessage(Func<T, string> messageQuery);
+        /// <summary>
+        /// Specifies a custom message to be used when the current condition fails.
+        /// </summary>
+        /// <param name="message">The message to be used for the condition failure.</param>
+        /// <returns>The current <see cref="IRuleBuilder{T}"/> instance.</returns>
+        IRuleBuilder<T> WithMessage(string message);
     }
 }
