6487b275b3ac4f2791c93c6e9e2bb581ab9e626e
tacosontitan <jkdavis1691@gmail.com>
2022-11-18 19:22:24 -0600

    Added some basic regions.

--- Mauve/Runtime/Processing/Rule.cs
+++ Mauve/Runtime/Processing/Rule.cs
@@ -3,5 +3,17 @@
     public class Rule
     {
 
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
     }
 }
