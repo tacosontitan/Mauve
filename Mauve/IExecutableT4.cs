54bc65571f5a461dd6153dae9089e54c320cc980
tacosontitan <jkdavis1691@gmail.com>
2022-11-20 10:56:28 -0600

    Added empty documentation to IExecutableT4.

--- Mauve/IExecutableT4.cs
+++ Mauve/IExecutableT4.cs
@@ -8,8 +8,33 @@ namespace Mauve
     /// </summary>
     public interface IExecutable<T1, T2, T3, T4, TOut>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input1"></param>
+        /// <param name="input2"></param>
+        /// <param name="input3"></param>
+        /// <param name="input4"></param>
+        /// <returns></returns>
         TOut Execute(T1 input1, T2 input2, T3 input3, T4 input4);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input1"></param>
+        /// <param name="input2"></param>
+        /// <param name="input3"></param>
+        /// <param name="input4"></param>
+        /// <returns></returns>
         Task<TOut> ExecuteAsync(T1 input1, T2 input2, T3 input3, T4 input4);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input1"></param>
+        /// <param name="input2"></param>
+        /// <param name="input3"></param>
+        /// <param name="input4"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<TOut> ExecuteAsync(T1 input1, T2 input2, T3 input3, T4 input4, CancellationToken cancellationToken);
     }
 }
