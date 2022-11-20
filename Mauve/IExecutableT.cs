5ed2190f6c3b7227be269653cd063d02d4fbc7f5
tacosontitan <jkdavis1691@gmail.com>
2022-11-20 10:55:27 -0600

    Updated documentation for IExecutableT.

--- Mauve/IExecutableT.cs
+++ Mauve/IExecutableT.cs
@@ -8,8 +8,21 @@ namespace Mauve
     /// </summary>
     public interface IExecutable<T, TOut>
     {
+        /// <summary>
+        /// Executes the <see cref="IExecutable"/>.
+        /// </summary>
+        /// <param name="input">The input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
         TOut Execute(T input);
+        /// <summary>
+        /// Executes the <see cref="IExecutable{T, TOut}"/> asynchronously.
+        /// </summary>
+        /// <param name="input">The input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
         Task<TOut> ExecuteAsync(T input);
+        /// <summary>
+        /// Executes the <see cref="IExecutable{T, TOut}"/> asynchronously.
+        /// </summary>
+        /// <param name="input">The input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to cancel execution.</param>
         Task<TOut> ExecuteAsync(T input, CancellationToken cancellationToken);
     }
 }
