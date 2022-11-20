7d79f7ff5e1a47af4569974277ea2534a29bb7b1
tacosontitan <jkdavis1691@gmail.com>
2022-11-20 11:02:04 -0600

    Updated documentation for IExecutableT4.

--- Mauve/IExecutableT4.cs
+++ Mauve/IExecutableT4.cs
@@ -11,28 +11,28 @@ namespace Mauve
         /// <summary>
         ///  Executes the <see cref="IExecutable"/>.
         /// </summary>
-        /// <param name="input1"></param>
-        /// <param name="input2"></param>
-        /// <param name="input3"></param>
-        /// <param name="input4"></param>
+        /// <param name="input1">The first input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
+        /// <param name="input2">The second input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
+        /// <param name="input3">The thrid input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
+        /// <param name="input4">The fourth input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
         /// <returns></returns>
         TOut Execute(T1 input1, T2 input2, T3 input3, T4 input4);
         /// <summary>
         /// Executes the <see cref="IExecutable"/> asynchronously.
         /// </summary>
-        /// <param name="input1"></param>
-        /// <param name="input2"></param>
-        /// <param name="input3"></param>
-        /// <param name="input4"></param>
+        /// <param name="input1">The first input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
+        /// <param name="input2">The second input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
+        /// <param name="input3">The thrid input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
+        /// <param name="input4">The fourth input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
         /// <returns></returns>
         Task<TOut> ExecuteAsync(T1 input1, T2 input2, T3 input3, T4 input4);
         /// <summary>
         /// Executes the <see cref="IExecutable"/> asynchronously.
         /// </summary>
-        /// <param name="input1"></param>
-        /// <param name="input2"></param>
-        /// <param name="input3"></param>
-        /// <param name="input4"></param>
+        /// <param name="input1">The first input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
+        /// <param name="input2">The second input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
+        /// <param name="input3">The thrid input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
+        /// <param name="input4">The fourth input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to cancel execution.</param>
         /// <returns></returns>
         Task<TOut> ExecuteAsync(T1 input1, T2 input2, T3 input3, T4 input4, CancellationToken cancellationToken);
