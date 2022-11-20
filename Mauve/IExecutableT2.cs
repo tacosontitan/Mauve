1bab95bde4814e9c70912608d9bef6d6a9bd2f10
tacosontitan <jkdavis1691@gmail.com>
2022-11-20 10:30:09 -0600

    Added IExecutableT2.

---
+++ Mauve/IExecutableT2.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mauve
+{
+    /// <summary>
+    /// Represents an <see langword="interface"/> that exposes an execute method which returns data.
+    /// </summary>
+    public interface IExecutable<TIn, TOut>
+    {
+        TOut Execute(TIn input);
+        Task<TOut> ExecuteAsync(TIn input);
+        Task<TOut> ExecuteAsync(TIn input, CancellationToken cancellationToken);
+    }
+}
