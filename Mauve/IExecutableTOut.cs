d0041e6fb03d98d6952ee5fe0e3617439fdd03bb
tacosontitan <jkdavis1691@gmail.com>
2022-11-20 10:48:48 -0600

    Created IExecutableTOut.

---
+++ Mauve/IExecutableTOut.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mauve
+{
+    /// <summary>
+    /// Represents an <see langword="interface"/> that exposes an execute method which returns data.
+    /// </summary>
+    public interface IExecutable<T>
+    {
+        T Execute();
+        Task<T> ExecuteAsync();
+        Task<T> ExecuteAsync(CancellationToken cancellationToken);
+    }
+}
