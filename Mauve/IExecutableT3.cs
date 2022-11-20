63a99669034c10c38227cbc8c9dba555b7aef9c2
tacosontitan <jkdavis1691@gmail.com>
2022-11-20 10:31:12 -0600

    Added IExecutableT3.

---
+++ Mauve/IExecutableT3.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mauve
+{
+    /// <summary>
+    /// Represents an <see langword="interface"/> that exposes an execute method which returns data.
+    /// </summary>
+    public interface IExecutable<TOut, T1, T2>
+    {
+        TOut Execute(T1 input1, T2 input2);
+        Task<TOut> ExecuteAsync(T1 input1, T2 input2);
+        Task<TOut> ExecuteAsync(T1 input1, T2 input2, CancellationToken cancellationToken);
+    }
+}
