b3b35224732521c9645d642fcc178007cffcf4c6
tacosontitan <jkdavis1691@gmail.com>
2022-11-20 10:52:16 -0600

    Updated documentation for IExecutable.

--- Mauve/IExecutable.cs
+++ Mauve/IExecutable.cs
@@ -8,8 +8,19 @@ namespace Mauve
     /// </summary>
     public interface IExecutable
     {
+        /// <summary>
+        /// Executes the <see cref="IExecutable"/>.
+        /// </summary>
         void Execute();
+        /// <summary>
+        /// Executes the <see cref="IExecutable"/> asynchronously.
+        /// </summary>
+        /// <returns></returns>
         Task ExecuteAsync();
+        /// <summary>
+        /// Executes the <see cref="IExecutable"/> asynchronously.
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to cancel execution.</param>
         Task ExecuteAsync(CancellationToken cancellationToken);
     }
 }
