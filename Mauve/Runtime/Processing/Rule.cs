7fcd60e8c7d6a76dbe2bdd0df3a3ad93f3cb5618
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 17:32:17 -0600

    Created abstract rule.

---
+++ Mauve/Runtime/Processing/Rule.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mauve.Runtime.Processing
+{
+    /// <summary>
+    /// Represents an <see langword="abstract"/> implementation of <see cref="IRule{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type this rule applies to.</typeparam>
+    public abstract class Rule<T> : IRule<T>
+    {
+        /// <summary>
+        /// Applies the rule to the specified input.
+        /// </summary>
+        /// <param name="input">The input to which the rule should be applied.</param>
+        public abstract void Apply(T input);
+        /// <summary>
+        /// Applies the rule asynchronously.
+        /// </summary>
+        /// <param name="input">The input to which the rule should be applied.</param>
+        /// <returns>A <see cref="Task"/> that describes the state of the application.</returns>
+        public async Task ApplyAsync(T input) =>
+            await ApplyAsync(input, CancellationToken.None);
+        /// <summary>
+        /// Applies the rule asynchronously.
+        /// </summary>
+        /// <param name="input">The input to which the rule should be applied.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> through which the application can be cancelled.</param>
+        /// <returns>A <see cref="Task"/> that describes the state of the application.</returns>
+        public async Task ApplyAsync(T input, CancellationToken cancellationToken) =>
+            await Task.Run(() => Apply(input), cancellationToken);
+    }
+}
