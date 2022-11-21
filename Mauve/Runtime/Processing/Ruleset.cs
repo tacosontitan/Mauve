94e66c10badce11ff3d40b6ed9b035b0f870b94c
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 17:24:22 -0600

    Created ruleset.

---
+++ Mauve/Runtime/Processing/Ruleset.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mauve.Runtime.Processing
+{
+    /// <summary>
+    /// Represents a collection of rules that should be applied in FIFO order.
+    /// </summary>
+    public class Ruleset<T> : IRule<T>
+    {
+
+        #region Fields
+
+        private readonly List<IRule<T>> _rules;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The rules which this <see cref="Ruleset"/> applies.
+        /// </summary>
+        public IReadOnlyCollection<IRule<T>> Rules => _rules.AsReadOnly();
+
+        #endregion
+
+        #region Public Methods
+
+        public void Add(IRule<T> rule) => _rules.Add(rule);
+        public void Apply(T input) => throw new System.NotImplementedException();
+        public Task ApplyAsync(T input) => throw new System.NotImplementedException();
+        public Task ApplyAsync(T input, CancellationToken cancellationToken) => throw new System.NotImplementedException();
+        public void Clear() => _rules.Clear();
+        public void Remove(IRule<T> rule) => _rules.Remove(rule);
+
+        #endregion
+
+    }
+}
