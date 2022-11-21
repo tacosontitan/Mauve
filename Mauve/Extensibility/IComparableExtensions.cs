ee1086f7da6beef918d5124c09de3e0b2e6aa6ac
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 16:05:26 -0600

    Added null handling to within range.

--- Mauve/Extensibility/IComparableExtensions.cs
+++ Mauve/Extensibility/IComparableExtensions.cs
@@ -32,6 +32,6 @@ namespace Mauve.Extensibility
         /// <param name="max">The maximum allowed value.</param>
         /// <returns>Returns true if the calling object's value is equal to or greater than the specified minimum value, while remaining less than or equal to the specified maximum value, otherwise false.</returns>
         public static bool WithinRange(this IComparable value, IComparable min, IComparable max) =>
-            value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+            value?.CompareTo(min) >= 0 && value?.CompareTo(max) <= 0;
     }
 }
