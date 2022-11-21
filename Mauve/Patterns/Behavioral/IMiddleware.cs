d8b65d865f1a451f906ddc3e6919129bcde3a017
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 15:43:43 -0600

    Updated documentation.

--- Mauve/Patterns/Behavioral/IMiddleware.cs
+++ Mauve/Patterns/Behavioral/IMiddleware.cs
@@ -17,13 +17,14 @@ namespace Mauve.Patterns
         /// Invokes the middleware.
         /// </summary>
         /// <param name="next">The next middleware to utilize.</param>
+        /// <returns>A <see cref="Task"/> describing the result of the invocation.</returns>
         Task InvokeAsync(MiddlewareDelegate next);
         /// <summary>
         /// Invokes the middleware.
         /// </summary>
         /// <param name="next">The next middleware to utilize.</param>
         /// <param name="cancellationToken">The cancellation token used to cancel asynchronous processing.</param>
-        /// <returns></returns>
+        /// <returns>A <see cref="Task"/> describing the result of the invocation.</returns>
         Task InvokeAsync(MiddlewareDelegate next, CancellationToken cancellationToken);
     }
 }
