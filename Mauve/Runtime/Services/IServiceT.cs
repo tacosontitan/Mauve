8722bdb8ebe0ef408478d94ef7a24d9148807df4
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:05:37 -0600

    Updated documentation.

--- Mauve/Runtime/Services/IServiceT.cs
+++ Mauve/Runtime/Services/IServiceT.cs
@@ -3,9 +3,19 @@ using Mauve.Patterns;
 
 namespace Mauve.Runtime.Services
 {
+    /// <summary>
+    /// An <see langword="interface"/> which represents a basic service.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type of data utilized in requests to this service.</typeparam>
     public interface IService<T>
     {
+        /// <summary>
+        /// A collection of dependencies for the service.
+        /// </summary>
         IDependencyCollection Dependencies { get; set; }
+        /// <summary>
+        /// The middleware delegate the service runs requests through.
+        /// </summary>
         MiddlewareDelegate<T> MiddlewareDelegate { get; set; }
     }
 }
