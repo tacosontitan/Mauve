4ceac123050e67cc5570eef71e383dfe744c57e3
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:56:05 -0600

    Removed methods from IServiceBuilderT6.

--- Mauve/Runtime/Services/IServiceBuilderT6.cs
+++ Mauve/Runtime/Services/IServiceBuilderT6.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-using Mauve.Patterns;
+﻿using Mauve.Patterns;
 
 namespace Mauve.Runtime.Services
 {
@@ -30,8 +28,6 @@ namespace Mauve.Runtime.Services
         /// <param name="instance">The instance to register.</param>
         /// <returns>Returns the current <see cref="IServiceBuilder{T1, T2, T3, T4, T5, T6}"/> instance.</returns>
         IServiceBuilder<T1, T2, T3, T4, T5, T6> AddSingleton<T>(T instance);
-        IServiceBuilder<T1, T2, T3, T4, T5, T6> AddSingleton(string alias, Type type, object instance);
-        IServiceBuilder<T1, T2, T3, T4, T5, T6> AddSingleton(Type type, object instance);
         void Run(IMiddleware<T1, T2, T3, T4, T5, T6> middleware);
         IServiceBuilder<T1, T2, T3, T4, T5, T6> Use(IMiddleware<T1, T2, T3, T4, T5, T6> middleware);
     }
