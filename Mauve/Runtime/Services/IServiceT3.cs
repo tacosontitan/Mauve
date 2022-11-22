5bd520879ea08ac1724b17388c0255bc517f3c15
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:08:40 -0600

    Simplified interface.

--- Mauve/Runtime/Services/IServiceT3.cs
+++ Mauve/Runtime/Services/IServiceT3.cs
@@ -3,9 +3,8 @@ using Mauve.Patterns;
 
 namespace Mauve.Runtime.Services
 {
-    public interface IService<T1, T2, T3>
+    public interface IService<T1, T2, T3> : IService
     {
-        IDependencyCollection Dependencies { get; set; }
-        MiddlewareDelegate<T1, T2, T3> MiddlewareDelegate { get; set; }
+        new MiddlewareDelegate<T1, T2, T3> MiddlewareDelegate { get; set; }
     }
 }
