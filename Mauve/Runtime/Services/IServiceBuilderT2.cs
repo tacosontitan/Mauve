0148c60272ae2d719e178cf86b9e56b917e58ab3
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:11:52 -0600

    Simplified IServiceBuilderT2.

--- Mauve/Runtime/Services/IServiceBuilderT2.cs
+++ Mauve/Runtime/Services/IServiceBuilderT2.cs
@@ -1,15 +1,9 @@
-﻿using System;
-
-using Mauve.Patterns;
+﻿using Mauve.Patterns;
 
 namespace Mauve.Runtime.Services
 {
-    public interface IServiceBuilder<T1, T2>
+    public interface IServiceBuilder<T1, T2> : IServiceBuilder
     {
-        IServiceBuilder AddSingleton<T>(string alias, T instance);
-        IServiceBuilder AddSingleton<T>(T instance);
-        IServiceBuilder AddSingleton(string alias, Type type, object instance);
-        IServiceBuilder AddSingleton(Type type, object instance);
         IServiceBuilder Run(IMiddleware<T1, T2> middleware);
         IServiceBuilder Use(IMiddleware<T1, T2> middleware);
     }
