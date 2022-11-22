53510eae1bd51a58f2d36f4ad5ea4fe1b49bf7e8
tacosontitan <jkdavis1691@gmail.com>
2022-11-22 13:44:56 -0600

    Added get methods to IServiceT.

--- Mauve/Runtime/Services/IServiceT.cs
+++ Mauve/Runtime/Services/IServiceT.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 using Mauve.Patterns;
 
 namespace Mauve.Runtime.Services
@@ -7,11 +8,41 @@ namespace Mauve.Runtime.Services
     /// An <see langword="interface"/> which represents a basic service.
     /// </summary>
     /// <typeparam name="T">Specifies the type of data utilized in requests to this service.</typeparam>
-    public interface IService<T> : IService
+    public interface IService<T>
     {
+        /// <summary>
+        /// A collection of dependencies for the service.
+        /// </summary>
+        IDependencyCollection Dependencies { get; set; }
         /// <summary>
         /// The middleware delegate the service runs requests through.
         /// </summary>
-        new MiddlewareDelegate<T> MiddlewareDelegate { get; set; }
+        MiddlewareDelegate<T> MiddlewareDelegate { get; set; }
+
+        #region Methods
+
+        /// <summary>
+        /// Gets a dependency from the service.
+        /// </summary>
+        /// <typeparam name="TOut">Specifies the type of dependency to get.</typeparam>
+        /// <returns>Returns the identified instance of the dependency.</returns>
+        TOut Get<TOut>();
+        /// <summary>
+        /// Gets a dependency from the service.
+        /// </summary>
+        /// <typeparam name="TOut">Specifies the type of dependency to get.</typeparam>
+        /// <param name="alias">The alias of the dependency.</param>
+        /// <returns>Returns the identified instance of the dependency.</returns>
+        TOut Get<TOut>(string alias);
+        /// <summary>
+        /// Gets a dependency from the service.
+        /// </summary>
+        /// <typeparam name="TOut">Specifies the base type of the dependency to get.</typeparam>
+        /// <param name="type">The specific type of dependency to get.</param>
+        /// <returns>Returns the identified instance of the dependency.</returns>
+        TOut Get<TOut>(Type type);
+
+        #endregion
+
     }
 }
