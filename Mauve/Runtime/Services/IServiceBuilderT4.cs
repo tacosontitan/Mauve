9034bb4d90f699bfb20d946ff5026c553e36c4bb
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:42:42 -0600

    Updated documentation.

--- Mauve/Runtime/Services/IServiceBuilderT4.cs
+++ Mauve/Runtime/Services/IServiceBuilderT4.cs
@@ -4,6 +4,13 @@ using Mauve.Patterns;
 
 namespace Mauve.Runtime.Services
 {
+    /// <summary>
+    /// An <see cref="interface"/> capable of building an <see cref="IService{T1, T2, T3, T4, T5, T6, T7, T8, T9}"/>.
+    /// </summary>
+    /// <typeparam name="T1">Specifies the first type of the service.</typeparam>
+    /// <typeparam name="T2">Specifies the second type of the service.</typeparam>
+    /// <typeparam name="T3">Specifies the third type of the service.</typeparam>
+    /// <typeparam name="T4">Specifies the fourth type of the service.</typeparam>
     public interface IServiceBuilder<T1, T2, T3, T4> : IBuilder<IService<T1, T2, T3, T4>>
     {
         IServiceBuilder<T1, T2, T3, T4> AddSingleton<T>(string alias, T instance);
