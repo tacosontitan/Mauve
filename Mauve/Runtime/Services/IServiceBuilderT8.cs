bd00a519e9dc2b2cf44a8ef895e2fbde217275c8
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:49:08 -0600

    Updated documentation.

--- Mauve/Runtime/Services/IServiceBuilderT8.cs
+++ Mauve/Runtime/Services/IServiceBuilderT8.cs
@@ -17,6 +17,13 @@ namespace Mauve.Runtime.Services
     /// <typeparam name="T8">Specifies the eighth type of the service.</typeparam>
     public interface IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8> : IBuilder<IService<T1, T2, T3, T4, T5, T6, T7, T8>>
     {
+        /// <summary>
+        /// Adds a singleton instance to the service using the specified alias.
+        /// </summary>
+        /// <typeparam name="T">Specifies the type of the singleton.</typeparam>
+        /// <param name="alias">The alias used to identify the specified instance.</param>
+        /// <param name="instance">The instance to register.</param>
+        /// <returns>Returns the current <see cref="IServiceBuilder{T1, T2, T3, T4, T5, T6, T7, T8}"/> instance.</returns>
         IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8> AddSingleton<T>(string alias, T instance);
         IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8> AddSingleton<T>(T instance);
         IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8> AddSingleton(string alias, Type type, object instance);
