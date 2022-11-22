fe5aaaf0e1384cd4b32d1cafeec97d605c381198
tacosontitan <jkdavis1691@gmail.com>
2022-11-22 13:44:06 -0600

    Created IMediator.

---
+++ Mauve/Patterns/Behavioral/IMediator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Mauve.Patterns.Behavioral
+{
+    public interface IMediator
+    {
+        T Send<T>(IMediatorRequest<T> request);
+        Task<T> SendAsync<T>(IMediatorRequest<T> request);
+    }
+}
