cb617cc25c859140c761508a54da9bab19ba3192
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 19:15:49 -0600

    Fleshed out INetworkConnection.

--- Mauve/Net/INetworkConnection.cs
+++ Mauve/Net/INetworkConnection.cs
@@ -1,18 +1,42 @@
-﻿namespace Mauve.Net
+﻿using System;
+using System.Data;
+
+namespace Mauve.Net
 {
     /// <summary>
-    /// 
+    /// An <see cref="interface"/> which represents a network connection.
     /// </summary>
-    public interface INetworkConnection
+    public interface INetworkConnection : IDisposable
     {
 
         #region Properties
 
-        NetworkConnectionInformation ConnectionInformation { get; set; }
+        /// <summary>
+        /// The <see cref="ConnectionState"/> of this <see cref="INetworkConnection"/>.
+        /// </summary>
+        ConnectionState State { get; }
+        /// <summary>
+        /// The connection information for this <see cref="INetworkConnection"/> instance.
+        /// </summary>
+        NetworkConnectionInformation ConnectionInformation { get; }
 
         #endregion
 
         #region Methods
+
+        /// <summary>
+        /// Opens a connection with the specified <see cref="NetworkConnectionInformation"/>.
+        /// </summary>
+        void Open();
+        /// <summary>
+        /// Closes the connection with the specified <see cref="NetworkConnectionInformation"/>.
+        /// </summary>
+        void Close();
+        /// <summary>
+        /// Tests the connection with the specified <see cref="NetworkConnectionInformation"/>.
+        /// </summary>
+        /// <param name="connectionSucceeded">Specifies whether or not the connection test was successful.</param>
+        void Test(out bool connectionSucceeded);
 
         #endregion
 
