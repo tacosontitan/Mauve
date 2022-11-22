2ac45323137a96aaca4c4b3e34e0d623d68ec33a
tacosontitan <jkdavis1691@gmail.com>
2022-11-22 15:20:11 -0600

    Reduced generics.

--- Mauve/Net/INetworkRequestBuilder.cs
+++ Mauve/Net/INetworkRequestBuilder.cs
@@ -4,47 +4,46 @@ using Mauve.Patterns;
 
 namespace Mauve.Net
 {
-    public interface INetworkRequestBuilder<TRequest, TData> : IBuilder<TRequest>
-        where TRequest : INetworkRequest<TData>
+    public interface INetworkRequestBuilder<T> : IBuilder<INetworkRequest<T>>
     {
         /// <summary>
         /// Authorizes the request using the specified <see cref="NetworkCredential"/>.
         /// </summary>
         /// <param name="networkCredential">The network credential to authorize with.</param>
         /// <returns>Returns the current builder instance.</returns>
-        INetworkRequestBuilder<TRequest, TData> Authorize(NetworkCredential networkCredential);
+        INetworkRequestBuilder<T> Authorize(NetworkCredential networkCredential);
         /// <summary>
         /// Sets the method of the request to <see cref="NetworkRequestMethod.Delete"/>.
         /// </summary>
         /// <returns>Returns the current builder instance.</returns>
-        INetworkRequestBuilder<TRequest, TData> Delete();
+        INetworkRequestBuilder<T> Delete();
         /// <summary>
         /// Sets the method of the request to <see cref="NetworkRequestMethod.Get"/>.
         /// </summary>
         /// <returns>Returns the current builder instance.</returns>
-        INetworkRequestBuilder<TRequest, TData> Get();
+        INetworkRequestBuilder<T> Get();
         /// <summary>
         /// Sets the method of the request to <see cref="NetworkRequestMethod.Patch"/>.
         /// </summary>
         /// <returns>Returns the current builder instance.</returns>
-        INetworkRequestBuilder<TRequest, TData> Patch();
+        INetworkRequestBuilder<T> Patch();
         /// <summary>
         /// Sets the method of the request to <see cref="NetworkRequestMethod.Post"/>.
         /// </summary>
         /// <returns>Returns the current builder instance.</returns>
-        INetworkRequestBuilder<TRequest, TData> Post();
+        INetworkRequestBuilder<T> Post();
         /// <summary>
         /// Sets the method of the request to <see cref="NetworkRequestMethod.Put"/>.
         /// </summary>
         /// <returns>Returns the current builder instance.</returns>
-        INetworkRequestBuilder<TRequest, TData> Put();
+        INetworkRequestBuilder<T> Put();
         /// <summary>
         /// Specifies where the request is going.
         /// </summary>
         /// <param name="uri">The host of the connection (e.g. the IP Address, Server Name, Base URL, etc).</param>
         /// <param name="port">The port this connection uses.</param>
         /// <returns>Returns the current builder instance.</returns>
-        INetworkRequestBuilder<TRequest, TData> Using(Uri uri, int? port = null);
+        INetworkRequestBuilder<T> Using(Uri uri, int? port = null);
         /// <summary>
         /// Adds the specified header to the <see cref="INetworkRequest{T}"/>.
         /// </summary>
@@ -52,12 +51,12 @@ namespace Mauve.Net
         /// <param name="key">The key for the header.</param>
         /// <param name="value">The value of the header.</param>
         /// <returns>Returns the current builder instance.</returns>
-        INetworkRequestBuilder<TRequest, TData> UseHeader<T>(string key, T value);
+        INetworkRequestBuilder<T> UseHeader<THeader>(string key, THeader value);
         /// <summary>
         /// Writes the specified <see cref="TData"/> to the request.
         /// </summary>
         /// <param name="data">The data to send with the request.</param>
         /// <returns>Returns the current builder instance.</returns>
-        INetworkRequestBuilder<TRequest, TData> Write(TData data);
+        INetworkRequestBuilder<T> Write(T data);
     }
 }
