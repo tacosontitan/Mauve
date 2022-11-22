93da47167fc169692f776ca105022d902176e6a8
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:08:25 -0600

    Simplified interface.

--- Mauve/Runtime/Services/IServiceT2.cs
+++ Mauve/Runtime/Services/IServiceT2.cs
@@ -8,15 +8,11 @@ namespace Mauve.Runtime.Services
     /// </summary>
     /// <typeparam name="T1">Specifies the first type of data utilized in requests to this service.</typeparam>
     /// <typeparam name="T2">Specifies the second type of data utilized in requests to this service.</typeparam>
-    public interface IService<T1, T2>
+    public interface IService<T1, T2> : IService
     {
-        /// <summary>
-        /// A collection of dependencies for the service.
-        /// </summary>
-        IDependencyCollection Dependencies { get; set; }
         /// <summary>
         /// The middleware delegate the service runs requests through.
         /// </summary>
-        MiddlewareDelegate<T1, T2> MiddlewareDelegate { get; set; }
+        new MiddlewareDelegate<T1, T2> MiddlewareDelegate { get; set; }
     }
 }
