a274ec4c40638d1950c9022549a7b2b6fbcd9d18
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:17:03 -0600

    Fixed return type in IServiceBuilderT9.

--- Mauve/Runtime/Services/IServiceBuilderT9.cs
+++ Mauve/Runtime/Services/IServiceBuilderT9.cs
@@ -6,11 +6,11 @@ namespace Mauve.Runtime.Services
 {
     public interface IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8, T9>
     {
-        IServiceBuilder AddSingleton<T>(string alias, T instance);
-        IServiceBuilder AddSingleton<T>(T instance);
-        IServiceBuilder AddSingleton(string alias, Type type, object instance);
-        IServiceBuilder AddSingleton(Type type, object instance);
-        IServiceBuilder Run(IMiddleware<T1, T2, T3, T4, T5, T6, T7, T8, T9> middleware);
-        IServiceBuilder Use(IMiddleware<T1, T2, T3, T4, T5, T6, T7, T8, T9> middleware);
+        IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8, T9> AddSingleton<T>(string alias, T instance);
+        IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8, T9> AddSingleton<T>(T instance);
+        IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8, T9> AddSingleton(string alias, Type type, object instance);
+        IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8, T9> AddSingleton(Type type, object instance);
+        IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8, T9> Run(IMiddleware<T1, T2, T3, T4, T5, T6, T7, T8, T9> middleware);
+        IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8, T9> Use(IMiddleware<T1, T2, T3, T4, T5, T6, T7, T8, T9> middleware);
     }
 }
