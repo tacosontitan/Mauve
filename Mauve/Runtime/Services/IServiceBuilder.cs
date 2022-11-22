a4eeb093e69ffe288e5527cb516d54d3b0ef4a3a
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:48:34 -0600

    Updated documentation.

--- Mauve/Runtime/Services/IServiceBuilder.cs
+++ Mauve/Runtime/Services/IServiceBuilder.cs
@@ -9,6 +9,13 @@ namespace Mauve.Runtime.Services
     /// </summary>
     public interface IServiceBuilder : IBuilder<IService>
     {
+        /// <summary>
+        /// Adds a singleton instance to the service using the specified alias.
+        /// </summary>
+        /// <typeparam name="T">Specifies the type of the singleton.</typeparam>
+        /// <param name="alias">The alias used to identify the specified instance.</param>
+        /// <param name="instance">The instance to register.</param>
+        /// <returns>Returns the current <see cref="IServiceBuilder"/> instance.</returns>
         IServiceBuilder AddSingleton<T>(string alias, T instance);
         IServiceBuilder AddSingleton<T>(T instance);
         IServiceBuilder AddSingleton(string alias, Type type, object instance);
