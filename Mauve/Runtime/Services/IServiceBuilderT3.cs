a832af5258ab9eed8f7cbed0e677123f217c0e8d
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:16:14 -0600

    Unsimplified and fixed return type in IServiceBuilderT3.

--- Mauve/Runtime/Services/IServiceBuilderT3.cs
+++ Mauve/Runtime/Services/IServiceBuilderT3.cs
@@ -1,10 +1,16 @@
-﻿using Mauve.Patterns;
+﻿using System;
+
+using Mauve.Patterns;
 
 namespace Mauve.Runtime.Services
 {
-    public interface IServiceBuilder<T1, T2, T3> : IServiceBuilder
+    public interface IServiceBuilder<T1, T2, T3>
     {
-        IServiceBuilder Run(IMiddleware<T1, T2, T3> middleware);
-        IServiceBuilder Use(IMiddleware<T1, T2, T3> middleware);
+        IServiceBuilder<T1, T2, T3> AddSingleton<T>(string alias, T instance);
+        IServiceBuilder<T1, T2, T3> AddSingleton<T>(T instance);
+        IServiceBuilder<T1, T2, T3> AddSingleton(string alias, Type type, object instance);
+        IServiceBuilder<T1, T2, T3> AddSingleton(Type type, object instance);
+        IServiceBuilder<T1, T2, T3> Run(IMiddleware<T1, T2, T3> middleware);
+        IServiceBuilder<T1, T2, T3> Use(IMiddleware<T1, T2, T3> middleware);
     }
 }
