251c944efdefff89aa901daf8c12f22f5ca9467d
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:43:48 -0600

    Updated documentation.

--- Mauve/Runtime/Services/IServiceBuilderT7.cs
+++ Mauve/Runtime/Services/IServiceBuilderT7.cs
@@ -5,7 +5,7 @@ using Mauve.Patterns;
 namespace Mauve.Runtime.Services
 {
     /// <summary>
-    /// An <see cref="interface"/> capable of building an <see cref="IService{T1, T2, T3, T4, T5, T6, T7, T8, T9}"/>.
+    /// An <see cref="interface"/> capable of building an <see cref="IService{T1, T2, T3, T4, T5, T6, T7}"/>.
     /// </summary>
     /// <typeparam name="T1">Specifies the first type of the service.</typeparam>
     /// <typeparam name="T2">Specifies the second type of the service.</typeparam>
