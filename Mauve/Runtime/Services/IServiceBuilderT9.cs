d0096783919fe16db1868ef30285485521046310
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:39:54 -0600

    Derived from IBuilder.

--- Mauve/Runtime/Services/IServiceBuilderT9.cs
+++ Mauve/Runtime/Services/IServiceBuilderT9.cs
@@ -4,7 +4,7 @@ using Mauve.Patterns;
 
 namespace Mauve.Runtime.Services
 {
-    public interface IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8, T9>
+    public interface IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8, T9> : IBuilder<IService<T1, T2, T3, T4, T5, T6, T7, T8, T9>>
     {
         IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8, T9> AddSingleton<T>(string alias, T instance);
         IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8, T9> AddSingleton<T>(T instance);
