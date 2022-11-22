11655c84a2cff56db20d67089ca677c58d97e258
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:25:12 -0600

    Renamed service builder to service designer.

---
+++ Mauve/Runtime/Services/IServiceDesigner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Mauve.Patterns;
+
+namespace Mauve.Runtime.Services
+{
+    public interface IServiceDesigner
+    {
+        IServiceDesigner AddSingleton<T>(string alias, T instance);
+        IServiceDesigner AddSingleton<T>(T instance);
+        IServiceDesigner AddSingleton(string alias, Type type, object instance);
+        IServiceDesigner AddSingleton(Type type, object instance);
+        IServiceDesigner Run(IMiddleware middleware);
+        IServiceDesigner Use(IMiddleware middleware);
+    }
+}
