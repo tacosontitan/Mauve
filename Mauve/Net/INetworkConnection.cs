17aa60e32a817b40e249e47811e351680f2063bf
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 19:10:29 -0600

    Created network connection interface.

---
+++ Mauve/Net/INetworkConnection.cs
@@ -0,0 +1,20 @@
+﻿namespace Mauve.Net
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface INetworkConnection
+    {
+
+        #region Properties
+
+        NetworkConnectionInformation ConnectionInformation { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+    }
+}
