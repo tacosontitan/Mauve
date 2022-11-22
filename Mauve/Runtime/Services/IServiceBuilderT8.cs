cfb16fb74acfc00409bfbd2a569495b7cd04d5f5
tacosontitan <jkdavis1691@gmail.com>
2022-11-21 18:34:57 -0600

    Changed return type of Run to void in IServiceBuilderT8.

--- Mauve/Runtime/Services/IServiceBuilderT8.cs
+++ Mauve/Runtime/Services/IServiceBuilderT8.cs
@@ -10,7 +10,7 @@ namespace Mauve.Runtime.Services
         IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8> AddSingleton<T>(T instance);
         IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8> AddSingleton(string alias, Type type, object instance);
         IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8> AddSingleton(Type type, object instance);
-        IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8> Run(IMiddleware<T1, T2, T3, T4, T5, T6, T7, T8> middleware);
+        void Run(IMiddleware<T1, T2, T3, T4, T5, T6, T7, T8> middleware);
         IServiceBuilder<T1, T2, T3, T4, T5, T6, T7, T8> Use(IMiddleware<T1, T2, T3, T4, T5, T6, T7, T8> middleware);
     }
 }
