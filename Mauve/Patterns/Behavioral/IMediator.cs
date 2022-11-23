ef845fb069f80aa5b54735a8cc08955811ed73ed
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 12:13:12 -0600

    Updated mediator interface.

--- Mauve/Patterns/Behavioral/IMediator.cs
+++ Mauve/Patterns/Behavioral/IMediator.cs
@@ -1,10 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace Mauve.Patterns.Behavioral
 {
     public interface IMediator
     {
         T Send<T>(IMediatorRequest<T> request);
-        Task<T> SendAsync<T>(IMediatorRequest<T> request);
+        Task<T> Send<T>(IMediatorRequest<T> request, CancellationToken cancellationToken);
     }
 }
