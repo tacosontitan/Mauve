8132a44c0d53304bfebb02381bd858953a1f4860
tacosontitan <jkdavis1691@gmail.com>
2022-11-22 18:46:06 -0600

    Created new network request builder interface.

---
+++ Mauve/Net/INetworkRequestBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Mauve.Patterns;
+
+namespace Mauve.Net
+{
+    public interface INetworkRequestBuilder : IBuilder<INetworkRequest>
+    {
+        INetworkRequestBuilder Authorize(string username, string password);
+        INetworkRequestBuilder Authorize(NetworkTokenType tokenType, string token);
+        INetworkRequestBuilder Delete(string uri);
+        INetworkRequestBuilder Delete(Uri uri);
+        INetworkRequestBuilder Get(string uri);
+        INetworkRequestBuilder Get(Uri uri);
+        INetworkRequestBuilder Patch(string uri);
+        INetworkRequestBuilder Patch(Uri uri);
+        INetworkRequestBuilder Post(string uri);
+        INetworkRequestBuilder Post(Uri uri);
+        INetworkRequestBuilder Put(string uri);
+        INetworkRequestBuilder Put(Uri uri);
+        INetworkRequestBuilder UsingPort(int port);
+        INetworkRequestBuilder WithHeader<T>(string key, T value);
+        INetworkRequestBuilder WithParameter<T>(string key, T value);
+        INetworkRequestBuilder Write<T>(T data);
+    }
+}
