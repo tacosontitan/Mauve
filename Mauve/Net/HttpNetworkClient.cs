a848fdcca05e737b4357f1136540dba26517fea4
tacosontitan <jkdavis1691@gmail.com>
2022-11-22 19:48:00 -0600

    Created HttpNetworkClient shell.

---
+++ Mauve/Net/HttpNetworkClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mauve.Net
+{
+    internal class HttpNetworkClient : INetworkClient
+    {
+        public void Send(INetworkRequest request) => throw new NotImplementedException();
+        public T Send<T>(INetworkRequest request) => throw new NotImplementedException();
+        public void Send<T>(INetworkRequest<T> request) => throw new NotImplementedException();
+        public TOut Send<TIn, TOut>(INetworkRequest<TIn> request) => throw new NotImplementedException();
+        public Task SendAsync(INetworkRequest request, CancellationToken cancellationToken) => throw new NotImplementedException();
+        public Task<T> SendAsync<T>(INetworkRequest request, CancellationToken cancellationToken) => throw new NotImplementedException();
+        public Task SendAsync<T>(INetworkRequest<T> request, CancellationToken cancellationToken) => throw new NotImplementedException();
+        public Task<TOut> SendAsync<TIn, TOut>(INetworkRequest<TIn> request, CancellationToken cancellationToken) => throw new NotImplementedException();
+    }
+}
