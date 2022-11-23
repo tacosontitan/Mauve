3ec9422670fc1d3149fabcf6df7a98e9a9963359
tacosontitan <jkdavis1691@gmail.com>
2022-11-22 18:45:48 -0600

    Created new network request interface.

---
+++ Mauve/Net/INetworkRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Mauve.Net
+{
+    public interface INetworkRequest
+    {
+        int? Port { get; set; }
+        Uri Uri { get; set; }
+        HttpMethod Method { get; set; }
+        Dictionary<string, object> Headers { get; set; }
+        Dictionary<string, object> Parameters { get; set; }
+    }
+}
