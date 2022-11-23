facba11afb87bb588e7cb99ba36bae4076d35826
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 15:41:55 -0600

    Created service builder.

---
+++ Mauve/Runtime/Services/ServiceBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Mauve.Patterns;
+
+namespace Mauve.Runtime.Services
+{
+    public class ServiceBuilder : IServiceBuilder
+    {
+        public IServiceBuilder AddScoped<T>() => throw new NotImplementedException();
+        public IServiceBuilder AddScoped<T>(string alias) => throw new NotImplementedException();
+        public IServiceBuilder AddScoped<T>(IFactory<T> factory) => throw new NotImplementedException();
+        public IServiceBuilder AddScoped<T>(string alias, IFactory<T> factory) => throw new NotImplementedException();
+        public IServiceBuilder AddScoped<T>(Func<T> factory) => throw new NotImplementedException();
+        public IServiceBuilder AddScoped<T>(string alias, Func<T> factory) => throw new NotImplementedException();
+        public IServiceBuilder AddSingleton<T>(T instance) => throw new NotImplementedException();
+        public IServiceBuilder AddSingleton<T>(string alias, T instance) => throw new NotImplementedException();
+        public IServiceBuilder AddTransient<T>() => throw new NotImplementedException();
+        public IServiceBuilder AddTransient<T>(string alias) => throw new NotImplementedException();
+        public IServiceBuilder AddTransient<T>(IFactory<T> factory) => throw new NotImplementedException();
+        public IServiceBuilder AddTransient<T>(string alias, IFactory<T> factory) => throw new NotImplementedException();
+        public IServiceBuilder AddTransient<T>(Func<T> factory) => throw new NotImplementedException();
+        public IServiceBuilder AddTransient<T>(string alias, Func<T> factory) => throw new NotImplementedException();
+        public IService Build() => throw new NotImplementedException();
+        public void Run(IMiddleware middleware) => throw new NotImplementedException();
+        public IServiceBuilder Use(IMiddleware middleware) => throw new NotImplementedException();
+    }
+}
