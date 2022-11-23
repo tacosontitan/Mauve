209f76b5cc36f03177daa882e3d543edb45bf54c
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 12:34:26 -0600

    Created IHandlerT.

---
+++ Mauve/IHandlerT.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mauve
+{
+    /// <summary>
+    /// An <see langword="interface"/> that exposes methods to handle input.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type of data used as input for the handler.</typeparam>
+    public interface IHandler<T>
+    {
+        void Handle(T input);
+        Task Handle(T input, CancellationToken cancellationToken);
+    }
+}
