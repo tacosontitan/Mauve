c88beb3c74bdd121d6b616f40df859205a89cc1e
tacosontitan <jkdavis1691@gmail.com>
2022-11-22 18:46:21 -0600

    Created new generic network request interface.

--- Mauve/Net/INetworkRequestT.cs
+++ Mauve/Net/INetworkRequestT.cs
@@ -1,40 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Mauve.Net
+﻿namespace Mauve.Net
 {
     /// <summary>
     /// Represents an <see cref="INetworkRequest{T}"/> instance utilized for requesting data over a network.
     /// </summary>
-    public interface INetworkRequest<T>
+    public interface INetworkRequest<T> : INetworkRequest
     {
-        /// <summary>
-        /// The credential to be utilized for connection.
-        /// </summary>
-        NetworkCredential Credentials { get; set; }
         /// <summary>
         /// The data for this specific request.
         /// </summary>
         T Data { get; set; }
-        /// <summary>
-        /// The headers utilized in the request.
-        /// </summary>
-        Dictionary<string, object> Headers { get; set; }
-        /// <summary>
-        /// The parameters for the request.
-        /// </summary>
-        Dictionary<string, object> Parameters { get; set; }
-        /// <summary>
-        /// The method utilized by this request.
-        /// </summary>
-        NetworkRequestMethod Method { get; set; }
-        /// <summary>
-        /// The port this connection uses.
-        /// </summary>
-        int? Port { get; set; }
-        /// <summary>
-        /// The host of the connection (e.g. the IP Address, Server Name, Base URL, etc).
-        /// </summary>
-        Uri Uri { get; set; }
     }
 }
