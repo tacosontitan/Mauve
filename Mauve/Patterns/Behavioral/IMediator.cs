2bef8ed43719c5585291d494d781db5c9322b900
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 12:47:36 -0600

    Opted for IRequest.

--- Mauve/Patterns/Behavioral/IMediator.cs
+++ Mauve/Patterns/Behavioral/IMediator.cs
@@ -5,7 +5,7 @@ namespace Mauve.Patterns.Behavioral
 {
     public interface IMediator
     {
-        T Send<T>(IMediatorRequest<T> request);
-        Task<T> Send<T>(IMediatorRequest<T> request, CancellationToken cancellationToken);
+        T Send<T>(IRequest<T> request);
+        Task<T> Send<T>(IRequest<T> request, CancellationToken cancellationToken);
     }
 }
