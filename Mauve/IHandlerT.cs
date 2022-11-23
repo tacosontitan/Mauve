8a513e5a76b89985ceb48ab0560fb7717402eaba
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 12:35:30 -0600

    Updated documentation.

--- Mauve/IHandlerT.cs
+++ Mauve/IHandlerT.cs
@@ -9,7 +9,17 @@ namespace Mauve
     /// <typeparam name="T">Specifies the type of data used as input for the handler.</typeparam>
     public interface IHandler<T>
     {
+        /// <summary>
+        /// Handles the input.
+        /// </summary>
+        /// <param name="input">The input to handle.</param>
         void Handle(T input);
+        /// <summary>
+        /// Handles the input.
+        /// </summary>
+        /// <param name="input">The input to handle.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> to allow consumers to cancel their request.</param>
+        /// <returns>A <see cref="Task"/> describing the state of the operation.</returns>
         Task Handle(T input, CancellationToken cancellationToken);
     }
 }
