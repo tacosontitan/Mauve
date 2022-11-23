6d47edf4ca2331de909f31186c9ab20dec8b4f3a
tacosontitan <jkdavis1691@gmail.com>
2022-11-22 18:58:06 -0600

    Removed URI focused methods.

--- Mauve/Net/INetworkRequestBuilder.cs
+++ Mauve/Net/INetworkRequestBuilder.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-using Mauve.Patterns;
+﻿using Mauve.Patterns;
 
 namespace Mauve.Net
 {
@@ -9,15 +7,10 @@ namespace Mauve.Net
         INetworkRequestBuilder Authorize(string username, string password);
         INetworkRequestBuilder Authorize(NetworkTokenType tokenType, string token);
         INetworkRequestBuilder Delete(string uri);
-        INetworkRequestBuilder Delete(Uri uri);
         INetworkRequestBuilder Get(string uri);
-        INetworkRequestBuilder Get(Uri uri);
         INetworkRequestBuilder Patch(string uri);
-        INetworkRequestBuilder Patch(Uri uri);
         INetworkRequestBuilder Post(string uri);
-        INetworkRequestBuilder Post(Uri uri);
         INetworkRequestBuilder Put(string uri);
-        INetworkRequestBuilder Put(Uri uri);
         INetworkRequestBuilder UsingPort(int port);
         INetworkRequestBuilder WithHeader<T>(string key, T value);
         INetworkRequestBuilder WithParameter<T>(string key, T value);
