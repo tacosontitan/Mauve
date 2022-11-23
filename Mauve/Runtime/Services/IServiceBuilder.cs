4562be7da6fd71480b5b55c0026d89ac2bf69fdd
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 15:41:38 -0600

    Added overloads to IServiceBuilder.

--- Mauve/Runtime/Services/IServiceBuilder.cs
+++ Mauve/Runtime/Services/IServiceBuilder.cs
@@ -1,4 +1,6 @@
-﻿using Mauve.Patterns;
+﻿using System;
+
+using Mauve.Patterns;
 
 namespace Mauve.Runtime.Services
 {
@@ -8,6 +10,13 @@ namespace Mauve.Runtime.Services
     public interface IServiceBuilder : IBuilder<IService>
     {
         /// <summary>
+        /// Adds a singleton instance to the service.
+        /// </summary>
+        /// <typeparam name="T">Specifies the type of the singleton.</typeparam>
+        /// <param name="instance">The instance to register.</param>
+        /// <returns>Returns the current <see cref="IServiceBuilder"/> instance.</returns>
+        IServiceBuilder AddSingleton<T>(T instance);
+        /// <summary>
         /// Adds a singleton instance to the service using the specified alias.
         /// </summary>
         /// <typeparam name="T">Specifies the type of the singleton.</typeparam>
@@ -15,13 +24,18 @@ namespace Mauve.Runtime.Services
         /// <param name="instance">The instance to register.</param>
         /// <returns>Returns the current <see cref="IServiceBuilder"/> instance.</returns>
         IServiceBuilder AddSingleton<T>(string alias, T instance);
-        /// <summary>
-        /// Adds a singleton instance to the service.
-        /// </summary>
-        /// <typeparam name="T">Specifies the type of the singleton.</typeparam>
-        /// <param name="instance">The instance to register.</param>
-        /// <returns>Returns the current <see cref="IServiceBuilder"/> instance.</returns>
-        IServiceBuilder AddSingleton<T>(T instance);
+        IServiceBuilder AddScoped<T>();
+        IServiceBuilder AddScoped<T>(string alias);
+        IServiceBuilder AddScoped<T>(IFactory<T> factory);
+        IServiceBuilder AddScoped<T>(string alias, IFactory<T> factory);
+        IServiceBuilder AddScoped<T>(Func<T> factory);
+        IServiceBuilder AddScoped<T>(string alias, Func<T> factory);
+        IServiceBuilder AddTransient<T>();
+        IServiceBuilder AddTransient<T>(string alias);
+        IServiceBuilder AddTransient<T>(IFactory<T> factory);
+        IServiceBuilder AddTransient<T>(string alias, IFactory<T> factory);
+        IServiceBuilder AddTransient<T>(Func<T> factory);
+        IServiceBuilder AddTransient<T>(string alias, Func<T> factory);
         void Run(IMiddleware middleware);
         IServiceBuilder Use(IMiddleware middleware);
     }
