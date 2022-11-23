dc79b806f04f4a1cdfe9e87cc1be89ab14edd190
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 11:12:58 -0600

    Created mediator implementaion shell, renamed factory.

---
+++ Mauve/Patterns/Behavioral/Mediator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Mauve.Patterns.Behavioral
+{
+    internal class Mediator : IMediator
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+        public T Send<T>(IMediatorRequest<T> request) => throw new NotImplementedException();
+        public Task<T> SendAsync<T>(IMediatorRequest<T> request) => throw new NotImplementedException();
+    }
+}
