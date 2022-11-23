6825ebe69f989019244b304fbd1f0851c9209722
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 13:07:47 -0600

    Updated documentation.

--- Mauve/IExecutableT2.cs
+++ Mauve/IExecutableT2.cs
@@ -6,6 +6,9 @@ namespace Mauve
     /// <summary>
     /// Represents an <see langword="interface"/> that exposes an execute method which returns data.
     /// </summary>
+    /// <typeparam name="T1">Specifies the first type of input for the operation.</typeparam>
+    /// <typeparam name="T2">Specifies the second type of input for the operation.</typeparam>
+    /// <typeparam name="TOut">Specifies the expected result type for the operation.</typeparam>
     public interface IExecutable<T1, T2, TOut>
     {
         /// <summary>
@@ -13,7 +16,7 @@ namespace Mauve
         /// </summary>
         /// <param name="input1">The first input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
         /// <param name="input2">The second input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
-        /// <returns></returns>
+        /// <returns>The result of the operation.</returns>
         TOut Execute(T1 input1, T2 input2);
         /// <summary>
         /// Executes the <see cref="IExecutable"/> asynchronously.
@@ -21,7 +24,7 @@ namespace Mauve
         /// <param name="input1">The first input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
         /// <param name="input2">The second input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to cancel execution.</param>
-        /// <returns></returns>
+        /// <returns>A <see cref="Task"/> describing the state of the operation.</returns>
         Task<TOut> Execute(T1 input1, T2 input2, CancellationToken cancellationToken);
     }
 }
