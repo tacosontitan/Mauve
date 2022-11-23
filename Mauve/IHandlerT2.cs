145e824d07a503ce32979521aad39d5cf259f26e
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 12:36:44 -0600

    Created IHandlerT2.

---
+++ Mauve/IHandlerT2.cs
@@ -0,0 +1,11 @@
+﻿namespace Mauve
+{
+    /// <summary>
+    /// An <see langword="interface"/> that exposes methods to handle input and return an output.
+    /// </summary>
+    /// <typeparam name="TIn">Specifies the type of data used as input for the handler.</typeparam>
+    /// <typeparam name="TOut">Specifies the type of data used as output for the handler.</typeparam>
+    public interface IHandler<TIn, TOut>
+    {
+    }
+}
