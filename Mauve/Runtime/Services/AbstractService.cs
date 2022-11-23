792d47ecc7b14479a88b5e9e74aaea332b5af21b
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 15:41:48 -0600

    Created abstract service.

---
+++ Mauve/Runtime/Services/AbstractService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mauve.Runtime.Services
+{
+    public abstract class AbstractService : IService
+    {
+        public abstract void Configure(IServiceBuilder service);
+        public TOut Get<TOut>() => throw new NotImplementedException();
+        public TOut Get<TOut>(string alias) => throw new NotImplementedException();
+        public TOut Get<TOut>(Type type) => throw new NotImplementedException();
+        public T Get<T>(Predicate<T> predicate) => throw new NotImplementedException();
+    }
+}
