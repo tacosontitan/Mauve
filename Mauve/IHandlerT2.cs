a9cb312d075ee6339b8915f0d36740ef6adf7a38
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 12:38:17 -0600

    Added methods and documentation.

--- Mauve/IHandlerT2.cs
+++ Mauve/IHandlerT2.cs
@@ -1,4 +1,7 @@
-﻿namespace Mauve
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mauve
 {
     /// <summary>
     /// An <see langword="interface"/> that exposes methods to handle input and return an output.
@@ -7,5 +10,18 @@
     /// <typeparam name="TOut">Specifies the type of data used as output for the handler.</typeparam>
     public interface IHandler<TIn, TOut>
     {
+        /// <summary>
+        /// Handles the input.
+        /// </summary>
+        /// <param name="input">The input to handle.</param>
+        /// <returns>The result of handling the input.</returns>
+        TOut Handle(TIn input);
+        /// <summary>
+        /// Handles the input.
+        /// </summary>
+        /// <param name="input">The input to handle.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> to allow consumers to cancel their request.</param>
+        /// <returns>A <see cref="Task"/> describing the state of the operation.</returns>
+        Task<TOut> Handle(TIn input, CancellationToken cancellationToken);
     }
 }
