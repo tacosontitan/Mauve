6698d3725f89b435249f9d7e26a6fc43953fc788
tacosontitan <jkdavis1691@gmail.com>
2022-11-22 19:05:30 -0600

    Created network client interface.

---
+++ Mauve/Net/INetworkClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mauve.Net
+{
+    public interface INetworkClient
+    {
+        T Send<T>(INetworkRequest request);
+        Task<T> Send<T>(INetworkRequest request, CancellationToken cancellationToken);
+    }
+}
