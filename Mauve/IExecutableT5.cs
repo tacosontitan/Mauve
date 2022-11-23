6abadc66ad45ebbd23f2efdaa80279bbc4d299cd
tacosontitan <jkdavis1691@gmail.com>
2022-11-23 13:02:28 -0600

    Simplified IExecutableT5.

--- Mauve/IExecutableT5.cs
+++ Mauve/IExecutableT5.cs
@@ -26,18 +26,8 @@ namespace Mauve
         /// <param name="input3">The thrid input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
         /// <param name="input4">The fourth input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
         /// <param name="input5">The fifth input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
-        /// <returns></returns>
-        Task<TOut> ExecuteAsync(T1 input1, T2 input2, T3 input3, T4 input4, T5 input5);
-        /// <summary>
-        /// Executes the <see cref="IExecutable"/> asynchronously.
-        /// </summary>
-        /// <param name="input1">The first input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
-        /// <param name="input2">The second input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
-        /// <param name="input3">The thrid input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
-        /// <param name="input4">The fourth input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
-        /// <param name="input5">The fifth input parameter for this <see cref="IExecutable{T, TOut}"/>.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> used to cancel execution.</param>
         /// <returns></returns>
-        Task<TOut> ExecuteAsync(T1 input1, T2 input2, T3 input3, T4 input4, T5 input5, CancellationToken cancellationToken);
+        Task<TOut> Execute(T1 input1, T2 input2, T3 input3, T4 input4, T5 input5, CancellationToken cancellationToken);
     }
 }
