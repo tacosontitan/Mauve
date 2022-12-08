7b3ce251f55f9685d3ff44d876f8ed6c9e3d26d8
tacosontitan <jkdavis1691@gmail.com>
2022-12-08 13:17:49 -0600

    Updated service pipeline to utilize a single argument middleware system.

--- Mauve/Runtime/Services/IServicePipeline.cs
+++ Mauve/Runtime/Services/IServicePipeline.cs
@@ -5,18 +5,18 @@ namespace Mauve.Runtime.Services
     /// <summary>
     /// An <see langword="interface"/> which represents a service pipeline.
     /// </summary>
-    public interface IServicePipeline
+    public interface IServicePipeline<T>
     {
         /// <summary>
         /// Adds a terminal <see cref="IMiddleware"/> to the pipeline.
         /// </summary>
         /// <param name="middleware"></param>
-        void Run(IMiddleware middleware);
+        void Run(IMiddleware<T> middleware);
         /// <summary>
         /// Adds a <see cref="IMiddleware"/> to the pipeline.
         /// </summary>
         /// <param name="middleware">The middleware to add.</param>
         /// <returns>The current <see cref="IServicePipeline"/> instance.</returns>
-        IServicePipeline Use(IMiddleware middleware);
+        IServicePipeline<T> Use(IMiddleware middleware);
     }
 }
