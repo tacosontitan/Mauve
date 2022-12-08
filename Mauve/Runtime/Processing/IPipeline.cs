7f9433aaae5a2c21cdb11ef254532744b08e446f
tacosontitan <jkdavis1691@gmail.com>
2022-12-08 13:26:11 -0600

    Added use and run methods.

--- Mauve/Runtime/Processing/IPipeline.cs
+++ Mauve/Runtime/Processing/IPipeline.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Mauve.Patterns;
 
 namespace Mauve.Runtime.Processing
 {
     internal interface IPipeline
     {
+        /// <summary>
+        /// Adds a terminal <see cref="IMiddleware"/> to the pipeline.
+        /// </summary>
+        /// <param name="middleware"></param>
+        void Run<T>(IMiddleware<T> middleware);
+        /// <summary>
+        /// Adds a <see cref="IMiddleware"/> to the pipeline.
+        /// </summary>
+        /// <param name="middleware">The middleware to add.</param>
+        /// <returns>The current <see cref="IPipeline"/> instance.</returns>
+        IPipeline Use<T>(IMiddleware<T> middleware);
     }
 }
