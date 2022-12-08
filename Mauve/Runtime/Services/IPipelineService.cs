66b1039e00a4221084919bb311f93b5b0a490ef4
tacosontitan <jkdavis1691@gmail.com>
2022-12-08 13:29:12 -0600

    Created pipeline service.

---
+++ Mauve/Runtime/Services/IPipelineService.cs
@@ -0,0 +1,15 @@
+﻿using Mauve.Patterns;
+using Mauve.Runtime.Processing;
+
+namespace Mauve.Runtime.Services
+{
+    internal interface IPipelineService
+    {
+        /// <summary>
+        /// Configures the service.
+        /// </summary>
+        /// <param name="dependencies">The <see cref="IDependencyCollection"/> maintained by the service.</param>
+        /// <param name="pipeline">The <see cref="IPipeline"/> utilized by the service to execute requests.</param>
+        void Configure(IDependencyCollection dependencies, IPipeline pipeline);
+    }
+}
