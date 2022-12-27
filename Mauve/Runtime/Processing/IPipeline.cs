1642750297544564bd71e38cf0ec383b42cf21a3
tacosontitan <jkdavis1691@gmail.com>
2022-12-27 16:11:53 -0600

    Added generics to pipeline interface.

--- Mauve/Runtime/Processing/IPipeline.cs
+++ Mauve/Runtime/Processing/IPipeline.cs
@@ -2,18 +2,18 @@
 
 namespace Mauve.Runtime.Processing
 {
-    public interface IPipeline
+    public interface IPipeline<T>
     {
         /// <summary>
-        /// Adds a terminal <see cref="IMiddleware"/> to the pipeline.
+        /// Adds a terminal <see cref="IMiddleware{T}"/> to the pipeline.
         /// </summary>
         /// <param name="middleware"></param>
-        void Run<T>(IMiddleware<T> middleware);
+        void Run(IMiddleware<T> middleware);
         /// <summary>
-        /// Adds a <see cref="IMiddleware"/> to the pipeline.
+        /// Adds a <see cref="IMiddleware{T}"/> to the pipeline.
         /// </summary>
         /// <param name="middleware">The middleware to add.</param>
-        /// <returns>The current <see cref="IPipeline"/> instance.</returns>
-        IPipeline Use<T>(IMiddleware<T> middleware);
+        /// <returns>The current <see cref="IPipeline{T}"/> instance.</returns>
+        IPipeline<T> Use(IMiddleware<T> middleware);
     }
 }
